e5b50e3b11d672bffe5679c3512645820ccdec41
ckm960411 <a01098956117@gmail.com>
2023-09-07 00:53:49 +0900

    study page header 마크업

--- src/app/study/page.tsx
+++ src/app/study/page.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
+import dashboardImage from '../../../public/img/study-dashboard.jpeg';
+import { TbDatabaseEdit } from 'react-icons/tb';
 
 export default function StudyPage() {
-  return <div className="max-w-screen-xl px-16px mx-auto">StudyPage</div>;
+  return (
+    <section>
+      <section className="bg-main relative">
+        <Image
+          src={dashboardImage.src}
+          alt="study dashboard"
+          width={1200}
+          height={400}
+          className="w-full h-460px object-cover opacity-70"
+          priority
+        />
+        <div className="absolute bottom-48px left-1/2 translate-x-[-50%] text-white max-w-screen-xl mx-auto w-full px-16px">
+          <h1 className="text-24px font-medium mt-8px flex items-center gap-8px">
+            <span>{`KMin's Dev Note`}</span>
+            <TbDatabaseEdit className="text-24px" />
+          </h1>
+          <p className="text-48px mt-24px">경민의 개발공부</p>
+        </div>
+      </section>
+      <section className="max-w-screen-xl mx-auto px-16px pt-24px relative">
+        <hr className="my-24px" />
+      </section>
+    </section>
+  );
 }

30da9bbac282856206bf8f60e545d75ebf54f108
ckm960411 <a01098956117@gmail.com>
2023-09-08 20:19:31 +0900

    Categories 마크업

--- src/app/study/page.tsx
+++ src/app/study/page.tsx
@@ -2,41 +2,9 @@ import React from 'react';
 import Image from 'next/image';
 import { TbDatabaseEdit } from 'react-icons/tb';
 import dashboardImage from '../../../public/img/study-dashboard.jpeg';
-import javascriptImg from '../../../public/img/tmp/javascript.png';
-import typescriptImg from '../../../public/img/tmp/typescript.png';
-import reactImg from '../../../public/img/tmp/react.png';
-import nextjsImg from '../../../public/img/tmp/nextjs.png';
-import CategoryCard from '@/components/study/CategoryCard';
-import { CategoryCardData } from '@/utils/types/study';
+import Categories from '@/components/study/Categories';
 
 export default function StudyPage() {
-  const categories: CategoryCardData[] = [
-    {
-      id: 1,
-      title: 'JavaScript 마스터',
-      thumbnail: javascriptImg.src,
-      totalPostCount: 22,
-    },
-    {
-      id: 2,
-      title: 'TypeScript 마스터',
-      thumbnail: typescriptImg.src,
-      totalPostCount: 15,
-    },
-    {
-      id: 3,
-      title: 'React 마스터',
-      thumbnail: reactImg.src,
-      totalPostCount: 17,
-    },
-    {
-      id: 4,
-      title: 'Next JS 마스터',
-      thumbnail: nextjsImg.src,
-      totalPostCount: 6,
-    },
-  ];
-
   return (
     <section>
       <section className="bg-main relative">
@@ -57,24 +25,7 @@ export default function StudyPage() {
         </div>
       </section>
       <section className="max-w-screen-xl mx-auto px-16px pt-24px relative">
-        <hr className="my-24px" />
-        <div className="min-h-[360px]">
-          <p className="text-26px font-bold">Language & Framework</p>
-          <div className="grid grid-cols-3 mt-24px gap-16px">
-            {categories.map((category) => (
-              <CategoryCard key={category.id} category={category} />
-            ))}
-          </div>
-        </div>
-        <hr className="my-24px" />
-        <div className="min-h-[360px]">
-          <p className="text-26px font-bold">Language & Framework</p>
-          <div className="grid grid-cols-3 mt-24px gap-16px">
-            {categories.map((category) => (
-              <CategoryCard key={category.id} category={category} />
-            ))}
-          </div>
-        </div>
+        <Categories />
       </section>
     </section>
   );
--- src/components/study/Categories.tsx
+++ src/components/study/Categories.tsx
@@ -1,5 +1,51 @@
 import React from 'react';
+import CategoryCard from './CategoryCard';
+import { CategoryCardData } from '@/utils/types/study';
+
+import javascriptImg from '../../../public/img/tmp/javascript.png';
+import typescriptImg from '../../../public/img/tmp/typescript.png';
+import reactImg from '../../../public/img/tmp/react.png';
+import nextjsImg from '../../../public/img/tmp/nextjs.png';
 
 export default function Categories() {
-  return <div>Categories</div>;
+  const categories: CategoryCardData[] = [
+    {
+      id: 1,
+      title: 'JavaScript 마스터',
+      thumbnail: javascriptImg.src,
+      totalPostCount: 22,
+    },
+    {
+      id: 2,
+      title: 'TypeScript 마스터',
+      thumbnail: typescriptImg.src,
+      totalPostCount: 15,
+    },
+    {
+      id: 3,
+      title: 'React 마스터',
+      thumbnail: reactImg.src,
+      totalPostCount: 17,
+    },
+    {
+      id: 4,
+      title: 'Next JS 마스터',
+      thumbnail: nextjsImg.src,
+      totalPostCount: 6,
+    },
+  ];
+
+  return (
+    <div>
+      <hr className="my-24px" />
+      <div className="min-h-[360px]">
+        <p className="text-26px font-bold">Language & Framework</p>
+        <div className="grid grid-cols-3 mt-24px gap-16px">
+          {categories.map((category) => (
+            <CategoryCard key={category.id} category={category} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }

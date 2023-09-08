c6ecb1598b91b263978dd574e3237f49da2d35d1
ckm960411 <a01098956117@gmail.com>
2023-09-08 21:28:42 +0900

    CategoryPage 마크업

---
+++ src/app/study/[categoryId]/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Image from 'next/image';
+import javascriptImg from '../../../../public/img/tmp/javascript.png';
+
+interface CategoryPageProps {
+  params: { categoryId: string };
+}
+export default function CategoryPage({
+  params: { categoryId },
+}: CategoryPageProps) {
+  const category = {
+    id: 1,
+    title: 'JavaScript 마스터',
+    description: `const JavaScript = () => { console.log('JavaScript 의 세계에 오신 것을 환영합니다.') }`,
+    thumbnail: javascriptImg,
+    totalPostCount: 22,
+  };
+
+  return (
+    <div>
+      <div
+        className="flex-center flex-col h-400px bg-blue-50"
+        style={{
+          background:
+            'linear-gradient(90deg, rgba(236,253,245,1) 0%, rgba(219,234,254,1) 100%)',
+        }}
+      >
+        <Image
+          src={category.thumbnail}
+          alt="study dashboard"
+          width={1200}
+          height={400}
+          className="w-200px h-200px object-contain"
+          priority
+        />
+        <div className="mt-32px px-16px flex-center flex-col gap-16px">
+          <h1 className="font-bold text-32px">{category.title}</h1>
+          <p className="font-semibold text-gray-700">{category.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}

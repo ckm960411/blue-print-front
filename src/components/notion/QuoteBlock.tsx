b5eccec272698d27e27a6efbc2cf97ac9b0031fc
ckm960411 <a01098956117@gmail.com>
2023-09-30 00:01:33 +0900

    QuoteBlock 마크업

--- src/components/notion/QuoteBlock.tsx
+++ src/components/notion/QuoteBlock.tsx
@@ -1,9 +1,32 @@
-import { Block } from "@/utils/types/notion";
+import { getColorByBlockColor } from "@/utils/common/notion";
+import { Block, BlockType } from "@/utils/types/notion";
 import React from "react";
 
 interface QuoteBlockProps {
   block: Block;
 }
 export default function QuoteBlock({ block }: QuoteBlockProps) {
-  return <div>QuoteBlock</div>;
+  if (block.type !== BlockType.quote) return <></>;
+
+  const { quote } = block;
+  const { rich_text } = quote;
+
+  if (!rich_text) return <></>;
+
+  return (
+    <div className="border-l-2 border-gray-800 py-4px pl-12px text-16px font-medium">
+      {rich_text.map(
+        ({ annotations: { bold, color, underline }, text: { content } }, i) => (
+          <span
+            key={i}
+            className={`${bold ? "font-bold" : "font-medium"} ${
+              underline ? "underline" : ""
+            } ${getColorByBlockColor(color)}`}
+          >
+            {content}
+          </span>
+        ),
+      )}
+    </div>
+  );
 }

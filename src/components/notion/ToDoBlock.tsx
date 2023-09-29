80177bb3745cc0e7523e65f2805bfe897e54527c
ckm960411 <a01098956117@gmail.com>
2023-09-30 04:47:44 +0900

    ToDoBlock 마크업

--- src/components/notion/ToDoBlock.tsx
+++ src/components/notion/ToDoBlock.tsx
@@ -1,9 +1,30 @@
-import { Block } from "@/utils/types/notion";
+import { Block, BlockType } from "@/utils/types/notion";
 import React from "react";
+import RichText from "./RichText";
+import { BsCheckSquare, BsCheckSquareFill } from "react-icons/bs";
 
 interface ToDoBlockProps {
   block: Block;
 }
 export default function ToDoBlock({ block }: ToDoBlockProps) {
-  return <div>ToDoBlock</div>;
+  if (block.type !== BlockType.to_do) return <></>;
+
+  const {
+    to_do: { checked, rich_text },
+  } = block;
+
+  if (!rich_text || !checked) return <></>;
+
+  return (
+    <p className="flex gap-8px">
+      <button className="h-20px w-20px flex-shrink-0">
+        {checked ? (
+          <BsCheckSquareFill className="text-20px text-main" />
+        ) : (
+          <BsCheckSquare className="text-20px text-main" />
+        )}
+      </button>
+      <RichText richText={rich_text} />
+    </p>
+  );
 }

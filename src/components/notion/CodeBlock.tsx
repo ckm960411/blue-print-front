7157785cb942cfb4ceec168442185761ed28e92c
ckm960411 <a01098956117@gmail.com>
2023-09-30 03:05:29 +0900

    CodeBlock 마크업

--- src/components/notion/CodeBlock.tsx
+++ src/components/notion/CodeBlock.tsx
@@ -1,9 +1,45 @@
-import { Block } from "@/utils/types/notion";
+"use client";
+
+import { Block, BlockType } from "@/utils/types/notion";
 import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface CodeBlockProps {
   block: Block;
 }
 export default function CodeBlock({ block }: CodeBlockProps) {
-  return <div>CodeBlock</div>;
+  if (block.type !== BlockType.code) return <></>;
+
+  const { code } = block;
+  const { rich_text } = code;
+
+  if (!rich_text) return <></>;
+
+  const convertedLanguage =
+    code.language === "typescript"
+      ? "tsx"
+      : code.language === "javascript"
+      ? "jsx"
+      : code.language;
+
+  return (
+    <div>
+      <SyntaxHighlighter
+        language={convertedLanguage}
+        style={oneDark}
+        wrapLongLines
+        customStyle={{
+          overflowX: "auto",
+          maxWidth: "100%",
+          whiteSpace: "pre-wrap",
+          wordWrap: "break-word",
+          borderRadius: "10px",
+          margin: 0,
+        }}
+      >
+        {rich_text.map((richText) => richText.text.content).join("")}
+      </SyntaxHighlighter>
+    </div>
+  );
 }

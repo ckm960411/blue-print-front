aa08821a860eb6a9a279926023d45d6f143089f0
ckm960411 <a01098956117@gmail.com>
2023-09-30 00:39:39 +0900

    NumberedListItemBlock 마크업

--- src/components/notion/NumberedListItemBlock.tsx
+++ src/components/notion/NumberedListItemBlock.tsx
@@ -1,5 +1,7 @@
-import { Block } from "@/utils/types/notion";
+import { getColorByBlockColor } from "@/utils/common/notion";
+import { Block, BlockType } from "@/utils/types/notion";
 import React from "react";
+import { BsDot } from "react-icons/bs";
 
 interface NumberedListItemBlockProps {
   block: Block;
@@ -7,5 +9,31 @@ interface NumberedListItemBlockProps {
 export default function NumberedListItemBlock({
   block,
 }: NumberedListItemBlockProps) {
-  return <div>NumberedListItemBlock</div>;
+  if (block.type !== BlockType.numbered_list_item) return <></>;
+
+  const { numbered_list_item } = block;
+  const { rich_text } = numbered_list_item;
+
+  if (!rich_text) return <></>;
+
+  return (
+    <p className="flex items-start">
+      <BsDot className="h-22px w-22px flex-shrink-0" />
+      <span>
+        {rich_text.map(({ annotations, text: { content } }, i) => {
+          const { bold, color, underline } = annotations;
+          return (
+            <span
+              key={i}
+              className={`${bold ? "font-bold" : ""} ${
+                underline ? "underline" : ""
+              } ${getColorByBlockColor(color)}`}
+            >
+              {content}
+            </span>
+          );
+        })}
+      </span>
+    </p>
+  );
 }

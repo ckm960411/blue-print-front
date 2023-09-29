8177d8abdc46cf2741c46f4e0ecf6fcb2ee48d63
ckm960411 <a01098956117@gmail.com>
2023-09-29 22:42:10 +0900

    CategoryPage 데이터 반영

--- src/app/study/[pageId]/page.tsx
+++ src/app/study/[pageId]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import javascriptImg from "../../../../public/img/tmp/javascript.png";
 import axios from "axios";
+import { getNotionBlockList, getNotionPageById } from "@/utils/services/notion";
 
 interface CategoryPageProps {
   params: { pageId: string };
@@ -9,48 +10,31 @@ interface CategoryPageProps {
 export default async function CategoryPage({
   params: { pageId },
 }: CategoryPageProps) {
-  const notion = await axios
-    .get(`http://localhost:3000/api/notion`)
-    .then((res) => res.data)
-    .catch((e) => console.log("e: ", e));
+  const pageData = await getNotionPageById(pageId);
+  const { results: blocks } = await getNotionBlockList(pageId);
 
-  const category = {
-    id: 1,
-    title: "JavaScript 마스터",
-    description: `const JavaScript = () => { console.log('JavaScript 의 세계에 오신 것을 환영합니다.') }`,
-    thumbnail: javascriptImg,
-    totalPostCount: 22,
-  };
+  const pageTitle = pageData.properties.title.title[0].text.content;
+  const pageIcon = pageData.icon?.file?.url ?? "";
 
   return (
     <div>
       <div
-        className="flex-center h-240px flex-col rounded-10px bg-blue-50 md:h-320px lg:h-400px"
+        className="flex-center h-240px flex-col gap-16px rounded-10px bg-blue-50 md:h-320px lg:h-400px lg:gap-32px"
         style={{
           background:
             "linear-gradient(90deg, rgba(236,253,245,1) 0%, rgba(219,234,254,1) 100%)",
         }}
       >
         <Image
-          src={category.thumbnail}
+          src={pageIcon}
           alt="study dashboard"
           width={1200}
           height={400}
           className="h-80px w-80px object-contain md:h-160px md:w-160px lg:h-200px lg:w-200px"
           priority
         />
-        <div className="flex-center mt-16px flex-col gap-16px px-16px lg:mt-32px">
-          <h1 className="text-22px font-bold md:text-32px">{category.title}</h1>
-          <p className="px-48px text-center font-semibold text-gray-700">
-            {category.description}
-          </p>
-        </div>
+        <h1 className="text-22px font-bold md:text-32px">{pageTitle}</h1>
       </div>
-      {Object.values(notion.block ?? {}).map((b: any, i: number) => (
-        <div key={i} className="text-24px font-bold hover:text-main">
-          {b?.value?.format?.page_icon} {b?.value?.properties?.title?.[0]}
-        </div>
-      ))}
     </div>
   );
 }

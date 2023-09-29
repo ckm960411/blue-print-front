6104d414a7fe980320b9935150242ff60dd7b772
ckm960411 <a01098956117@gmail.com>
2023-09-29 22:56:15 +0900

    QuoteBlock 생성

--- src/components/notion/NotionBlock.tsx
+++ src/components/notion/NotionBlock.tsx
@@ -1,6 +1,7 @@
 import { Block, BlockType } from "@/utils/types/notion";
 import React from "react";
 import ParagraphBlock from "./ParagraphBlock";
+import QuoteBlock from "./QuoteBlock";
 
 interface NotionBlockProps {
   block: Block;
@@ -10,6 +11,10 @@ export default function NotionBlock({ block }: NotionBlockProps) {
 
   if (type === BlockType.pragraph) {
     return <ParagraphBlock block={block} />;
+  }
+
+  if (type === BlockType.quote) {
+    return <QuoteBlock block={block} />;
   }
 
   return <div>NotionBlock</div>;
---
+++ src/components/notion/QuoteBlock.tsx
@@ -0,0 +1,9 @@
+import { Block } from "@/utils/types/notion";
+import React from "react";
+
+interface QuoteBlockProps {
+  block: Block;
+}
+export default function QuoteBlock({ block }: QuoteBlockProps) {
+  return <div>QuoteBlock</div>;
+}

f070a5042e29a898bba8a72264eb742dd32c2619
ckm960411 <a01098956117@gmail.com>
2023-09-30 04:32:54 +0900

    인라인 코드블록 추가

--- src/components/notion/RichText.tsx
+++ src/components/notion/RichText.tsx
@@ -9,7 +9,19 @@ export default function RichText({ richText }: RichTextProps) {
   return (
     <span>
       {richText.map(({ annotations, text: { content } }, i) => {
-        const { bold, color, underline } = annotations;
+        const { bold, color, underline, code } = annotations;
+
+        if (code) {
+          return (
+            <code
+              key={i}
+              className="rounded-md bg-blue-50 px-4px py-2px text-14px font-medium text-main"
+            >
+              {content}
+            </code>
+          );
+        }
+
         return (
           <span
             key={i}

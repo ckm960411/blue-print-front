fc8afd875f563c9b097af9e40ef7ebf24201d329
ckm960411 <a01098956117@gmail.com>
2023-09-30 12:27:15 +0900

    ToggleBlock 마크업

--- src/components/notion/ToggleBlock.tsx
+++ src/components/notion/ToggleBlock.tsx
@@ -1,9 +1,34 @@
+"use client";
+
 import { Block } from "@/utils/types/notion";
-import React from "react";
+import React, { useState } from "react";
+import RichText from "./RichText";
+import { TbTriangleInvertedFilled } from "react-icons/tb";
 
 interface ToggleBlockProps {
   block: Block;
 }
 export default function ToggleBlock({ block }: ToggleBlockProps) {
-  return <div>ToggleBlock</div>;
+  const [open, setOpen] = useState(false);
+
+  const { toggle } = block;
+  const { rich_text } = toggle;
+
+  if (!rich_text) return <></>;
+
+  return (
+    <div className="flex gap-8px">
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className={`flex-center h-24px w-24px duration-200 ${
+          open ? "-rotate-180" : ""
+        }`}
+      >
+        <TbTriangleInvertedFilled />
+      </button>
+      <p>
+        <RichText richText={rich_text} />
+      </p>
+    </div>
+  );
 }

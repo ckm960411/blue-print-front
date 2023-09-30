3c899eb4f1d4fb2942d0a1c7b617440260938477
ckm960411 <a01098956117@gmail.com>
2023-09-30 13:00:17 +0900

    ImageBlock 마크업

--- src/components/notion/ImageBlock.tsx
+++ src/components/notion/ImageBlock.tsx
@@ -1,9 +1,23 @@
 import { Block } from "@/utils/types/notion";
+import Image from "next/image";
 import React from "react";
+import RichText from "./RichText";
 
 interface ImageBlockProps {
   block: Block;
 }
 export default function ImageBlock({ block }: ImageBlockProps) {
-  return <div>ImageBlock</div>;
+  const { image } = block;
+
+  const url = image?.file?.url ?? "";
+  const caption = image?.caption ?? [];
+
+  return (
+    <div>
+      <Image src={url} alt="" width={1024} height={720} className="w-full" />
+      <p className="mt-8px text-14px text-gray-600">
+        <RichText richText={caption} />
+      </p>
+    </div>
+  );
 }

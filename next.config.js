2482c2b1679e9c33c67e930f1cedf3804d77a7a4
ckm960411 <a01098956117@gmail.com>
2023-09-30 16:28:38 +0900

    update next config

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 's3.us-west-2.amazonaws.com'
       },
+      {
+        protocol: 'https',
+        hostname: '*'
+      },
     ]
   }
 }

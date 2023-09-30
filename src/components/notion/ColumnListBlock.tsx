e5992ed78a60bd204018ed534fa34f7115e756de
ckm960411 <a01098956117@gmail.com>
2023-10-01 01:46:10 +0900

    ColumnListBlock 구현

--- src/components/notion/ColumnListBlock.tsx
+++ src/components/notion/ColumnListBlock.tsx
@@ -1,9 +1,22 @@
+import { getNotionBlockList } from "@/utils/services/notion";
 import { Block } from "@/utils/types/notion";
 import React from "react";
+import ColumnBlock from "./ColumnBlock";
 
 interface ColumnListBlockProps {
   block: Block;
 }
-export default function ColumnListBlock({ block }: ColumnListBlockProps) {
-  return <div>ColumnListBlock</div>;
+export default async function ColumnListBlock({ block }: ColumnListBlockProps) {
+  const { results: columns } = await getNotionBlockList(block.id);
+
+  return (
+    <div
+      className="grid"
+      style={{ gridTemplateColumns: `repeat(${columns.length}, 1fr)` }}
+    >
+      {columns.map((col) => (
+        <ColumnBlock key={col.id} block={block} />
+      ))}
+    </div>
+  );
 }

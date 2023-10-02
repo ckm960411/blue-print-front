d779844f43f41e1a5a6b432b03670cce5e7b09a4
ckm960411 <a01098956117@gmail.com>
2023-10-03 05:15:13 +0900

    code refactoring

--- src/utils/services/index.ts
+++ src/utils/services/index.ts
@@ -1,1 +1,4 @@
-export const API_BASE_URL = typeof window === 'undefined' ? process.env.API_BASE_URL : process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_BASE_URL =
+  typeof window === "undefined"
+    ? process.env.API_BASE_URL
+    : process.env.NEXT_PUBLIC_API_BASE_URL;

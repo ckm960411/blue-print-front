ddf29e7867d6912828c8333ded34205d2eb5b7e2
경민 <a01098956117@gmail.com>
2023-10-02 23:08:14 +0900

    변수명 수정

--- src/components/notion/ColumnListBlock.tsx
+++ src/components/notion/ColumnListBlock.tsx
@@ -1,21 +1,21 @@
 import { getNotionBlockList } from "@/utils/services/notion";
 import { Block } from "@/utils/types/notion";
-import React from "react";
+import React, { useEffect } from "react";
 import ColumnBlock from "./ColumnBlock";
 
 interface ColumnListBlockProps {
   block: Block;
 }
 export default async function ColumnListBlock({ block }: ColumnListBlockProps) {
-  const { results: columns } = await getNotionBlockList(block.id);
+  const { results: blocks } = await getNotionBlockList(block.id);
 
   return (
     <div
-      className="grid gap-16px py-16px"
-      style={{ gridTemplateColumns: `repeat(${columns.length}, 1fr)` }}
+      className="grid gap-16px pb-8px"
+      style={{ gridTemplateColumns: `repeat(${blocks.length}, 1fr)` }}
     >
-      {columns.map((col, index) => (
-        <ColumnBlock key={col.id} block={block} order={index + 1} />
+      {blocks.map((block, index) => (
+        <ColumnBlock key={block.id} block={block} order={index + 1} />
       ))}
     </div>
   );

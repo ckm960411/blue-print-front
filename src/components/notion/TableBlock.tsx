55c8ac51389ac574e7d2f364d9aa45ec4376a261
ckm960411 <a01098956117@gmail.com>
2023-10-03 16:00:58 +0900

    TableBlock 가로스크롤 생성

--- src/components/notion/TableBlock.tsx
+++ src/components/notion/TableBlock.tsx
@@ -1,18 +1,38 @@
+"use client";
+
 import { getNotionBlockList } from "@/utils/services/notion";
-import { Block, BlockType } from "@/utils/types/notion";
+import { Block } from "@/utils/types/notion";
 import { Table, TableContainer, Tbody, Th, Tr } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RichText from "./RichText";
+import { useWindowSize } from "usehooks-ts";
+import { useMediaQuery } from "react-responsive";
 
 interface TableBlockProps {
   block: Block;
 }
-export default async function TableBlock({ block }: TableBlockProps) {
-  const { results: rows } = await getNotionBlockList(block.id);
+export default function TableBlock({ block }: TableBlockProps) {
+  const { width } = useWindowSize();
+  const UNDER_480PX = useMediaQuery({ query: "(max-width: 479px)" });
+  const [rows, setRows] = useState<Block[]>([]);
+  const [maxWidth, setMaxWidth] = useState(0);
+
+  useEffect(() => {
+    getNotionBlockList(block.id)
+      .then(({ results }) => setRows(results))
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    setMaxWidth(width - 32 - (UNDER_480PX ? 0 : 88));
+  }, [width]);
 
   return (
-    <div className="my-8px rounded-10px border border-gray-200 px-8px">
-      <TableContainer>
+    <div
+      className="my-8px overflow-x-auto rounded-10px border border-gray-200 px-8px"
+      style={{ width: maxWidth }}
+    >
+      <TableContainer minWidth={800}>
         <Table variant="simple">
           <Tbody>
             {rows.map((tr) => (

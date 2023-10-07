6ef866d805faee2cfc4c77cfa4a5158bfb1a8c0c
ckm960411 <a01098956117@gmail.com>
2023-10-08 02:50:29 +0900

    WorkPage ProjectSelect 생성

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import ProjectSelect from "@/components/work/ProjectSelect";
 
 export default function WorkPage() {
-  return <div className="mx-auto max-w-screen-xl px-16px">WorkPage</div>;
+  return (
+    <div className="mx-auto max-w-screen-xl p-16px">
+      <div className="flex items-center gap-16px">
+        <h1 className="text-28px font-bold">My Work</h1>
+        <ProjectSelect />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/work/ProjectSelect.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
+import { useBoolean, useOnClickOutside } from "usehooks-ts";
+
+const projects = ["프로젝트1", "프로젝트2", "프로젝트3", "프로젝트4"];
+
+interface ProjectSelectProps {}
+export default function ProjectSelect({}: ProjectSelectProps) {
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const {
+    value: dropdownOpened,
+    setTrue: openDropdown,
+    setFalse: closeDropdown,
+  } = useBoolean(false);
+  const [project, setProject] = useState(projects[0]);
+
+  useOnClickOutside(dropdownRef, closeDropdown);
+
+  return (
+    <div className="relative">
+      <button
+        onClick={dropdownOpened ? closeDropdown : openDropdown}
+        className="flex items-center justify-between gap-8px rounded-10px bg-white p-8px hover:bg-gray-50"
+      >
+        <span className="truncate-1-lines w-100px text-left text-14px">
+          {project}
+        </span>
+        <FiChevronDown />
+      </button>
+      {dropdownOpened && (
+        <div className="absolute left-0 top-[calc(100%+8px)] w-200px rounded-10px border border-gray-200 bg-white py-8px shadow-md">
+          {projects.map((prj, i) => {
+            return (
+              <div
+                key={i}
+                ref={dropdownRef}
+                className={`w-full cursor-pointer p-8px text-14px hover:bg-gray-50 ${
+                  prj === project ? "bg-gray-50" : "bg-white"
+                }`}
+              >
+                {prj}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}

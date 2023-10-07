df87554f83dda588b501376753b05f907a219b90
ckm960411 <a01098956117@gmail.com>
2023-10-07 23:38:56 +0900

    PageIdAndUrl -> SectionCategory 타입명 수정

--- src/app/api/notion/pages/route.ts
+++ src/app/api/notion/pages/route.ts
@@ -1,11 +1,5 @@
 import { NextResponse } from "next/server";
 
-export interface PageIdAndUrl {
-  title: string;
-  page_id: string;
-  url: string;
-}
-
 export const GET = async () => {
   try {
     return NextResponse.json([
--- src/utils/types/study/index.ts
+++ src/utils/types/study/index.ts
@@ -1,7 +1,11 @@
-import { PageIdAndUrl } from "@/app/api/notion/pages/route";
+export interface SectionCategory {
+  title: string;
+  page_id: string;
+  url: string;
+}
 
 export interface CategorySection {
   id: number;
   name: string;
-  categories: PageIdAndUrl[];
+  categories: SectionCategory[];
 }

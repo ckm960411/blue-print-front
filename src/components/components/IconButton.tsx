d66649a6b6bab5bec47e9a64dd814053ff7d6ab4
경민 <a01098956117@gmail.com>
2023-10-09 14:08:52 +0900

    IconButton twMerge 사용

--- src/components/components/IconButton.tsx
+++ src/components/components/IconButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonProps {
   children?: React.ReactNode;
@@ -11,11 +12,16 @@ function IconButton(
   { children, w = 32, h, className, onClick }: IconButtonProps,
   ref: React.ForwardedRef<HTMLButtonElement>,
 ) {
+  const mergedClassName = twMerge(
+    "flex-center flex rounded-10px bg-white duration-200 hover:bg-gray-50",
+    className,
+  );
+
   return (
     <button
       ref={ref}
       onClick={onClick}
-      className={`flex-center flex rounded-10px bg-white duration-200 hover:bg-gray-50 ${className}`}
+      className={mergedClassName}
       style={{ width: w, height: h ?? w }}
     >
       {children}

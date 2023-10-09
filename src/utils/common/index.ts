ca584edfabe9ba8c2ce5ba8bf66d6d84d476e90f
경민 <a01098956117@gmail.com>
2023-10-09 18:35:56 +0900

    Color 변수 생성

--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -2,7 +2,7 @@ import { getDay } from "date-fns";
 
 export const Z_INDEX = {
   HEADER: 100,
-};
+} as const;
 
 export const getDayByAsiaSeoulFormat = (date: Date) => {
   return ["일", "월", "화", "수", "목", "금", "토"][getDay(new Date(date))];

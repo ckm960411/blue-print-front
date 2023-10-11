a7098706fc2153fadaf4340be089a41e0941e2af
ckm960411 <a01098956117@gmail.com>
2023-10-12 01:10:43 +0900

    TaskCard 생성

--- src/components/work/project-plan/sidetab/task/DrawerTodoContainer.tsx
+++ src/components/work/project-plan/sidetab/task/DrawerTodoContainer.tsx
@@ -1,24 +1,39 @@
-import React from "react";
-import { AiOutlineUnorderedList } from "react-icons/ai";
-import { BiFlag } from "react-icons/bi";
-import { IoHammerOutline } from "react-icons/io5";
-import DrawerTodoGroup from "@/components/work/project-plan/sidetab/task/DrawerTodoGroup";
+import TaskCard from "@/components/work/project-plan/sidetab/task/TaskCard";
+import React, { useState } from "react";
 
 interface DrawerTodoContainerProps {}
 export default function DrawerTodoContainer({}: DrawerTodoContainerProps) {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const tabs = [
+    { title: "To Do", borderColor: "border-orange-400" },
+    { title: "In Progress", borderColor: "border-blue-400" },
+    { title: "Review", borderColor: "border-purple-400" },
+    { title: "Completed", borderColor: "border-green-400" },
+  ];
+
   return (
     <div className="mt-16px flex flex-col gap-24px">
-      <DrawerTodoGroup
-        groupName="TO DO"
-        icon={<AiOutlineUnorderedList />}
-        theme="bg-teal-500"
-      />
-      <DrawerTodoGroup
-        groupName="IN PROGRESS"
-        icon={<IoHammerOutline />}
-        theme="bg-purple-500"
-      />
-      <DrawerTodoGroup groupName="DONE" icon={<BiFlag />} theme="bg-blue-500" />
+      <div className="grid grid-cols-4 gap-8px text-center text-gray-800">
+        {tabs.map((tab, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => setTabIndex(index)}
+              className={`border-b-[3px] py-4px ${tab.borderColor} ${
+                index === tabIndex
+                  ? "font-semibold text-gray-800"
+                  : "text-gray-400"
+              }`}
+            >
+              {tab.title}
+            </button>
+          );
+        })}
+      </div>
+      <div className="flex flex-col gap-16px">
+        <TaskCard />
+      </div>
     </div>
   );
 }

65e28e0d70b429eabf245484fee087b71d124aaf
ckm960411 <a01098956117@gmail.com>
2023-10-11 21:31:52 +0900

    프로젝트 버튼 추가

--- src/components/work/project-plan/ProjectPlanTab.tsx
+++ src/components/work/project-plan/ProjectPlanTab.tsx
@@ -1,12 +1,20 @@
 import { TabPanel } from "@chakra-ui/tabs";
 import React from "react";
 import MilestoneCard from "@/components/work/project-plan/MilestoneCard";
+import { AiOutlinePlus } from "react-icons/ai";
 
 interface ProjectPlanTabProps {}
 export default function ProjectPlanTab({}: ProjectPlanTabProps) {
   return (
     <TabPanel>
-      <p className="text-22px font-bold">Milestone</p>
+      <div className="flex-between">
+        <p className="text-22px font-bold">Milestone</p>
+        {/* TODO: 버튼 클릭시 마일스톤 추가 */}
+        <button className="flex-center gap-8px rounded-md px-8px py-6px text-14px font-medium duration-200 hover:bg-gray-50">
+          <AiOutlinePlus />
+          추가하기
+        </button>
+      </div>
 
       <hr className="my-16px" />
 

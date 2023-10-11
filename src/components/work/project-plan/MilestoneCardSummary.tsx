9209056d5a462b20c37df3b4d29986f0965593c8
ckm960411 <a01098956117@gmail.com>
2023-10-11 21:22:07 +0900

    MilestoneLinks 생성 및 마크업

--- src/components/work/project-plan/MilestoneCardSummary.tsx
+++ src/components/work/project-plan/MilestoneCardSummary.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Link from "next/link";
 import MilestoneStartDate from "@/components/work/project-plan/MilestoneStartDate";
 import MilestoneEndDate from "@/components/work/project-plan/MilestoneEndDate";
 import MilestoneClassification from "@/components/work/project-plan/MilestoneClassification";
 import MilestonePriority from "@/components/work/project-plan/MilestonePriority";
 import MilestoneProgress from "@/components/work/project-plan/MilestoneProgress";
 import MilestoneTags from "@/components/work/project-plan/MilestoneTags";
+import MilestoneLinks from "@/components/work/project-plan/MilestoneLinks";
 
 interface MilestoneCardSummaryProps {
   startDate: Date | undefined;
@@ -34,27 +34,7 @@ export default function MilestoneCardSummary({
       <MilestonePriority />
       <MilestoneProgress />
       <MilestoneTags />
-      <div className="flex items-start gap-8px">
-        <p className="truncate-1-lines w-80px text-14px font-medium text-gray-600">
-          링크
-        </p>
-        <div className="flex flex-col gap-12px">
-          <Link
-            href="https://uglyus.co.kr"
-            target="_blank"
-            className="text-14px text-blue-600 underline"
-          >
-            노션 기획 문서
-          </Link>
-          <Link
-            href="https://uglyus.co.kr"
-            target="_blank"
-            className="text-14px text-blue-600 underline"
-          >
-            피그마
-          </Link>
-        </div>
-      </div>
+      <MilestoneLinks />
     </div>
   );
 }
---
+++ src/components/work/project-plan/MilestoneLinks.tsx
@@ -0,0 +1,106 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import MilestoneEditButton from "@/components/work/project-plan/tooltip-button/MilestoneEditButton";
+import { Popover, PopoverContent, PopoverTrigger } from "@chakra-ui/react";
+
+export interface MilestoneLink {
+  id: number;
+  name: string;
+  href: string;
+}
+interface MilestoneLinksProps {}
+export default function MilestoneLinks({}: MilestoneLinksProps) {
+  const [links, setLinks] = useState<MilestoneLink[]>([]);
+  const [editing, setEditing] = useState(false);
+  const [tempLinkName, setTempLinkName] = useState("");
+  const [tempLinkHref, setTempLinkHref] = useState("");
+
+  const resetState = () => {
+    setTempLinkName("");
+    setTempLinkHref("");
+  };
+
+  const handleOpen = () => setEditing(true);
+  const handleClose = () => {
+    resetState();
+    setEditing(false);
+  };
+
+  const handleConfirm = () => {
+    // TODO: 링크 추가시 DB 반영후 revalidate 필요
+
+    handleClose();
+  };
+
+  return (
+    <div className="flex items-start gap-8px">
+      <p className="truncate-1-lines w-80px text-14px font-medium text-gray-600">
+        링크
+      </p>
+      <Popover isOpen={editing} onClose={handleClose} placement="bottom-start">
+        <PopoverTrigger>
+          <div className="flex items-start gap-16px">
+            <div className="flex flex-col gap-12px">
+              <Link
+                href="https://uglyus.co.kr"
+                target="_blank"
+                className="text-14px text-blue-600 underline"
+              >
+                노션 기획 문서
+              </Link>
+              <Link
+                href="https://uglyus.co.kr"
+                target="_blank"
+                className="text-14px text-blue-600 underline"
+              >
+                피그마
+              </Link>
+            </div>
+            <MilestoneEditButton
+              w={24}
+              onClick={handleOpen}
+              className="text-14px"
+              tooltipPlacement="right-start"
+            />
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-240px p-16px">
+          <div className="flex flex-col gap-16px text-14px">
+            <div className="flex items-center gap-8px">
+              <p className="flex-shrink-0 text-gray-600">이름:</p>
+              <input
+                value={tempLinkName}
+                placeholder="링크 이름 입력"
+                onChange={(e) => setTempLinkName(e.target.value)}
+                className="grow"
+              />
+            </div>
+            <div className="flex items-center gap-8px">
+              <p className="flex-shrink-0 text-gray-600">링크:</p>
+              <input
+                value={tempLinkHref}
+                placeholder="링크 주소 입력"
+                onChange={(e) => setTempLinkHref(e.target.value)}
+                className="grow"
+              />
+            </div>
+            <div className="flex items-center justify-end gap-8px">
+              <button
+                onClick={handleClose}
+                className="rounded-md px-8px py-6px text-14px font-medium text-gray-600 hover:bg-gray-50"
+              >
+                취소
+              </button>
+              <button
+                onClick={handleConfirm}
+                className="rounded-md px-8px py-6px text-14px font-medium text-gray-600 hover:bg-gray-50"
+              >
+                확인
+              </button>
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}

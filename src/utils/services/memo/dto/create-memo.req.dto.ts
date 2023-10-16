3c1a2f137fd0c210a5c57694699f9979cc81e473
ckm960411 <a01098956117@gmail.com>
2023-10-17 00:39:56 +0900

    createMemo API 생성

---
+++ src/utils/services/memo/dto/create-memo.req.dto.ts
@@ -0,0 +1,7 @@
+import { ColorKey } from "@/utils/common/color";
+
+export interface CreateMemoReqDto {
+  title: string;
+  content: string;
+  color?: ColorKey;
+}
---
+++ src/utils/services/memo/index.ts
@@ -0,0 +1,12 @@
+import { get, post } from "@/app/api/axios";
+import { CreateMemoReqDto } from "@/utils/services/memo/dto/create-memo.req.dto";
+import { Memo } from "@/utils/types/memo";
+
+export const getAllMemos = async () => {
+  return await get(`memo`);
+};
+
+export const createMemo = async (createMemoReqDto: CreateMemoReqDto) => {
+  const { data } = await post<Memo>(`memo`, createMemoReqDto);
+  return data;
+};

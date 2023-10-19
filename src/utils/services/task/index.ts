1f0366e791bfc94382ed9264d792af98cda572cd
ckm960411 <a01098956117@gmail.com>
2023-10-20 02:49:29 +0900

    add getAllTask API

--- src/utils/services/task/index.ts
+++ src/utils/services/task/index.ts
@@ -1,5 +1,12 @@
-import { post } from "@/app/api/axios";
+import { get, post } from "@/app/api/axios";
 import { CreateTaskReqDto } from "@/utils/services/task/dto/create-task.req.dto";
+import { Progress } from "@/utils/types";
+import { Task } from "@/utils/types/task";
+
+export const getAllTask = async (params?: { progress: Progress }) => {
+  const { data } = await get<Task[]>(`task`, { params });
+  return data;
+};
 
 export const createTask = async (createTaskReqDto: CreateTaskReqDto) => {
   const { data } = await post(`task`, createTaskReqDto);

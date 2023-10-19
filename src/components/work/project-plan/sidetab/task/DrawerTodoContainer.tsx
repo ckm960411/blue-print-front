cbde316a2d5b5600286149fe96e82164c4b11af3
ckm960411 <a01098956117@gmail.com>
2023-10-20 02:50:08 +0900

    상태별 task 목록 불러오기

--- src/components/work/project-plan/sidetab/task/DrawerTodoContainer.tsx
+++ src/components/work/project-plan/sidetab/task/DrawerTodoContainer.tsx
@@ -1,37 +1,68 @@
 import TaskCard from "@/components/work/project-plan/sidetab/task/TaskCard";
-import React, { useState } from "react";
+import { QueryKeys } from "@/utils/common/query-keys";
+import { getAllTask } from "@/utils/services/task";
+import { Progress } from "@/utils/types";
+import { useQuery } from "@tanstack/react-query";
+import { Toast } from "primereact/toast";
+import React, { useRef, useState } from "react";
 
 interface DrawerTodoContainerProps {}
 export default function DrawerTodoContainer({}: DrawerTodoContainerProps) {
-  const [tabIndex, setTabIndex] = useState(0);
+  const toast = useRef<Toast>(null);
+  const [progress, setProgress] = useState<Progress>(Progress.ToDo);
 
   const tabs = [
-    { title: "To Do", borderColor: "border-orange-400" },
-    { title: "In Progress", borderColor: "border-blue-400" },
-    { title: "Review", borderColor: "border-purple-400" },
-    { title: "Completed", borderColor: "border-green-400" },
+    { id: Progress.ToDo, title: "To Do", borderColor: "border-orange-400" },
+    {
+      id: Progress.InProgress,
+      title: "In Progress",
+      borderColor: "border-blue-400",
+    },
+    { id: Progress.Review, title: "Review", borderColor: "border-purple-400" },
+    {
+      id: Progress.Completed,
+      title: "Completed",
+      borderColor: "border-green-400",
+    },
   ];
 
+  const { data: tasks } = useQuery(
+    QueryKeys.getAllTasks(progress),
+    () => getAllTask({ progress }),
+    {
+      onError: () => {
+        toast.current?.show({
+          severity: "error",
+          summary: "문제 발생",
+          detail: "할일 목록을 불러오던 중 문제가 발생했습니다.",
+        });
+      },
+    },
+  );
+
   return (
-    <div className="mt-16px flex flex-col gap-24px">
-      <div className="grid grid-cols-4 gap-8px text-center text-gray-800">
-        {tabs.map((tab, index) => (
-          <button
-            key={index}
-            onClick={() => setTabIndex(index)}
-            className={`border-b-[3px] py-4px ${tab.borderColor} ${
-              index === tabIndex
-                ? "font-semibold text-gray-800"
-                : "text-gray-400"
-            }`}
-          >
-            {tab.title}
-          </button>
-        ))}
+    <>
+      <Toast ref={toast} />
+      <div className="mt-16px flex flex-col gap-24px">
+        <div className="grid grid-cols-4 gap-8px text-center text-gray-800">
+          {tabs.map((tab, index) => (
+            <button
+              key={index}
+              onClick={() => setProgress(tab.id)}
+              className={`border-b-[3px] py-4px ${tab.borderColor} ${
+                progress === tab.id
+                  ? "font-semibold text-gray-800"
+                  : "text-gray-400"
+              }`}
+            >
+              {tab.title}
+            </button>
+          ))}
+        </div>
+        <div className="flex flex-col gap-16px">
+          {tasks?.map((task) => <TaskCard key={task.id} />)}
+        </div>
       </div>
-      <div className="flex flex-col gap-16px">
-        <TaskCard />
-      </div>
-    </div>
+    </>
   );
 }
--- src/utils/common/query-keys.ts
+++ src/utils/common/query-keys.ts
@@ -1,7 +1,14 @@
+import { Progress } from "@/utils/types";
+
 export const GET_ALL_MEMOS = "getAllMemos";
 const getAllMemos = (showChecked?: boolean) => [GET_ALL_MEMOS, showChecked];
+
+const getAllTasks = (progress?: Progress) => ["get-all-tasks", progress];
 
 export const QueryKeys = {
   // MEMO
   getAllMemos,
+
+  // TASK
+  getAllTasks,
 };

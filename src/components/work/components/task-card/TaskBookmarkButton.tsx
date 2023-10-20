a09052b21c8f7604b73c2174c01fbbe42ccf32f4
ckm960411 <a01098956117@gmail.com>
2023-10-21 00:18:55 +0900

    add onSuccess & onError

--- src/components/work/components/task-card/TaskBookmarkButton.tsx
+++ src/components/work/components/task-card/TaskBookmarkButton.tsx
@@ -1,6 +1,8 @@
+import { QueryKeys } from "@/utils/common/query-keys";
 import { updateTask } from "@/utils/services/task";
-import { useMutation } from "@tanstack/react-query";
-import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Toast } from "primereact/toast";
+import React, { useRef } from "react";
 import { BsFillBookmarkFill } from "react-icons/bs";
 
 interface TaskBookmarkButtonProps {
@@ -11,25 +13,39 @@ export default function TaskBookmarkButton({
   taskId,
   isBookmarked,
 }: TaskBookmarkButtonProps) {
+  const toast = useRef<Toast>(null);
+  const queryClient = useQueryClient();
+
   const { mutate: bookmarkRequest } = useMutation(
     ["update-task"],
     () => updateTask(taskId, { isBookmarked: !isBookmarked }),
     {
-      onSuccess: (res) => console.log("res: ", res),
-      onError: console.error,
+      onSuccess: (res) => {
+        queryClient.invalidateQueries(QueryKeys.getAllTasks());
+      },
+      onError: () => {
+        toast.current?.show({
+          severity: "error",
+          summary: "문제 발생",
+          detail: "북마크 중 문제가 발생했습니다.",
+        });
+      },
     },
   );
 
   return (
-    <button
-      onClick={bookmarkRequest}
-      className="absolute right-8px top-0 px-8px pb-8px"
-    >
-      <BsFillBookmarkFill
-        className={`text-20px ${
-          isBookmarked ? "text-red-500" : "text-gray-300"
-        }`}
-      />
-    </button>
+    <>
+      <Toast ref={toast} />
+      <button
+        onClick={() => bookmarkRequest()}
+        className="absolute right-8px top-0 px-8px pb-8px"
+      >
+        <BsFillBookmarkFill
+          className={`text-20px ${
+            isBookmarked ? "text-red-500" : "text-gray-300"
+          }`}
+        />
+      </button>
+    </>
   );
 }

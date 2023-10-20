2ae95a7874762a07603235063de4432638595204
ckm960411 <a01098956117@gmail.com>
2023-10-20 21:21:59 +0900

    QueryKeys 업데이트

--- src/utils/common/query-keys.ts
+++ src/utils/common/query-keys.ts
@@ -1,9 +1,9 @@
 import { Progress } from "@/utils/types";
 
 export const GET_ALL_MEMOS = "getAllMemos";
-const getAllMemos = (showChecked?: boolean) => [GET_ALL_MEMOS, showChecked];
+const getAllMemos = (...args: any[]) => [GET_ALL_MEMOS, ...args];
 
-const getAllTasks = (progress?: Progress) => ["get-all-tasks", progress];
+const getAllTasks = (...args: any[]) => ["get-all-tasks", ...args];
 
 export const QueryKeys = {
   // MEMO

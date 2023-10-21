7e24e10bde4fb214d5acffeea87b2282e888733d
ckm960411 <a01098956117@gmail.com>
2023-10-22 01:10:53 +0900

    UrgentTaskContainer 생성

--- src/components/work/TaskListTab.tsx
+++ src/components/work/TaskListTab.tsx
@@ -2,6 +2,7 @@
 
 import TaskListTabHeader from "@/components/work/project-plan/TaskListTabHeader";
 import TaskContainer from "@/components/work/TaskContainer";
+import UrgentTaskContainer from "@/components/work/UrgentTaskContainer";
 import { TabPanel } from "@chakra-ui/tabs";
 import React from "react";
 
@@ -10,17 +11,7 @@ export default function TaskListTab({}: TaskListTabProps) {
   return (
     <TabPanel className="flex flex-col gap-16px">
       <TaskListTabHeader />
-
-      <div className="relative mt-16px flex flex-col gap-16px rounded-10px border border-red-200 px-16px pb-16px pt-32px">
-        <p className="absolute -top-16px left-8px bg-white p-8px text-18px font-bold text-red-500">
-          긴급한 태스크
-        </p>
-        <div className="flex flex-col gap-16px">
-          {/*<TaskCard />*/}
-          {/*<TaskCard />*/}
-        </div>
-      </div>
-
+      <UrgentTaskContainer />
       <TaskContainer />
     </TabPanel>
   );
---
+++ src/components/work/UrgentTaskContainer.tsx
@@ -0,0 +1,29 @@
+import TaskCard from "@/components/work/components/TaskCard";
+import { QueryKeys } from "@/utils/common/query-keys";
+import { getAllUrgenttTasks } from "@/utils/services/task";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+
+interface UrgentTaskContainerProps {}
+export default function UrgentTaskContainer({}: UrgentTaskContainerProps) {
+  const { data: tasks = [] } = useQuery(
+    QueryKeys.getAllTasks("urgent"),
+    getAllUrgenttTasks,
+    { onError: console.error },
+  );
+
+  if (tasks.length === 0) return <></>;
+
+  return (
+    <div className="relative mt-16px flex flex-col gap-16px rounded-10px border border-red-200 px-16px pb-16px pt-32px">
+      <p className="absolute -top-16px left-8px bg-white p-8px text-18px font-bold text-red-500">
+        긴급한 태스크
+      </p>
+      <div className="flex flex-col gap-16px">
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+    </div>
+  );
+}

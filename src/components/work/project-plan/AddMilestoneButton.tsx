9de42e7d198abead43ec98c7f7b620a823587bc7
ckm960411 <a01098956117@gmail.com>
2023-10-22 02:11:53 +0900

    milestone create 구현

--- src/components/work/project-plan/AddMilestoneButton.tsx
+++ src/components/work/project-plan/AddMilestoneButton.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { createMilestone } from "@/utils/services/milestone";
+import { useMutation } from "@tanstack/react-query";
 import React from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
 interface AddMilestoneButtonProps {}
 export default function AddMilestoneButton({}: AddMilestoneButtonProps) {
+  const { mutate: createMilestoneRequest } = useMutation(
+    ["create-milestone"],
+    () => createMilestone(),
+    {
+      onSuccess: console.log,
+      onError: console.error,
+    },
+  );
+
   return (
-    // TODO: 버튼 클릭시 마일스톤 추가
-    <button className="flex-center gap-8px rounded-md px-8px py-6px text-14px font-medium duration-200 hover:bg-gray-50">
+    <button
+      onClick={createMilestoneRequest}
+      className="flex-center gap-8px rounded-md px-8px py-6px text-14px font-medium duration-200 hover:bg-gray-50"
+    >
       <AiOutlinePlus />
       추가하기
     </button>

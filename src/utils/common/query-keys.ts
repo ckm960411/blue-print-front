4ed48ba7defe5d9f972093b378db676b18943a03
ckm960411 <a01098956117@gmail.com>
2023-10-22 02:34:53 +0900

    add getAllMilestones API

--- src/utils/common/query-keys.ts
+++ src/utils/common/query-keys.ts
@@ -5,10 +5,15 @@ const getAllMemos = (...args: any[]) => [GET_ALL_MEMOS, ...args];
 
 const getAllTasks = (...args: any[]) => ["get-all-tasks", ...args];
 
+const getAllMilestones = (...args: any[]) => ["get-all-milestones, ...args"];
+
 export const QueryKeys = {
   // MEMO
   getAllMemos,
 
   // TASK
   getAllTasks,
+
+  // MILESTONE
+  getAllMilestones,
 };
--- src/utils/services/milestone/index.ts
+++ src/utils/services/milestone/index.ts
@@ -1,10 +1,14 @@
-import { post } from "@/app/api/axios";
+import { get, post } from "@/app/api/axios";
 import { Milestone } from "@/utils/types/milestone";
 
+export const getAllMilestones = async () => {
+  const { data } = await get<Milestone[]>(`milestone`);
+  return data;
+};
+
 export const createMilestone = async () => {
-  const { data } = await post<Omit<Milestone, "tags" | "links" | "tasks">>(
-    `milestone`,
-    "",
-  );
+  const { data } = await post<
+    Omit<Milestone, "tags" | "links" | "tasks" | "memos">
+  >(`milestone`, "");
   return data;
 };

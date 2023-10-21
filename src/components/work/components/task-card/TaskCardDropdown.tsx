d3b5e3bb25abb2cedbcc98f19ece73b760eac734
ckm960411 <a01098956117@gmail.com>
2023-10-22 00:21:06 +0900

    Task delete 구현

--- src/components/work/components/task-card/TaskCardDropdown.tsx
+++ src/components/work/components/task-card/TaskCardDropdown.tsx
@@ -1,7 +1,12 @@
 import IconButton from "@/components/components/IconButton";
 import CreateUpdateTaskModal from "@/components/work/project-plan/CreateUpdateTaskModal";
+import { QueryKeys } from "@/utils/common/query-keys";
+import { deleteTask } from "@/utils/services/task";
 import { Task } from "@/utils/types/task";
 import { useDisclosure } from "@chakra-ui/hooks";
+import { Popover, PopoverContent, PopoverTrigger } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Toast } from "primereact/toast";
 import React, { useRef } from "react";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { BsPencil, BsTrash } from "react-icons/bs";
@@ -12,12 +17,19 @@ interface TaskCardDropdownProps {
 }
 export default function TaskCardDropdown({ task }: TaskCardDropdownProps) {
   const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const toast = useRef<Toast>(null);
 
+  const queryClient = useQueryClient();
   const {
     value: dropdownOpened,
     setTrue: openDropdown,
     setFalse: closeDropdown,
   } = useBoolean(false);
+  const {
+    isOpen: isDeletePopupOpen,
+    onOpen: openDeletePopup,
+    onClose: closeDeletePopup,
+  } = useDisclosure();
   const {
     isOpen: isModalOpen,
     onOpen: openModal,
@@ -25,21 +37,51 @@ export default function TaskCardDropdown({ task }: TaskCardDropdownProps) {
   } = useDisclosure();
   useOnClickOutside(dropdownRef, closeDropdown);
 
+  const { mutate: deleteTaskRequest } = useMutation(
+    ["delete-task"],
+    (id: number) => deleteTask(id),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.getAllTasks());
+      },
+      onError: (e: any) => {
+        toast.current?.show({
+          severity: "error",
+          summary: "문제 발생",
+          detail:
+            e?.response?.data?.message ||
+            "태스크 삭제 중 문제가 발생했습니다. 다시 시도해주세요.",
+        });
+      },
+    },
+  );
+
+  const handleUpdate = () => {
+    closeDropdown();
+    openModal();
+  };
+
+  const handleDelete = () => {
+    closeDropdown();
+    openDeletePopup();
+  };
+
   const menus = [
     {
       title: "수정하기",
       Icon: () => <BsPencil />,
-      onClick: openModal,
+      onClick: handleUpdate,
     },
     {
       title: "삭제하기",
       Icon: () => <BsTrash />,
-      onClick: () => {},
+      onClick: handleDelete,
     },
   ];
 
   return (
     <>
+      <Toast ref={toast} />
       <CreateUpdateTaskModal
         task={task}
         isOpen={isModalOpen}
@@ -48,12 +90,34 @@ export default function TaskCardDropdown({ task }: TaskCardDropdownProps) {
       />
 
       <div className="relative">
-        <IconButton
-          onClick={openDropdown}
-          className="bg-white hover:bg-gray-50"
-        >
-          <BiDotsVerticalRounded />
-        </IconButton>
+        <Popover isOpen={isDeletePopupOpen} placement="bottom-end">
+          <PopoverTrigger>
+            <IconButton
+              onClick={openDropdown}
+              className="bg-white hover:bg-gray-50"
+            >
+              <BiDotsVerticalRounded />
+            </IconButton>
+          </PopoverTrigger>
+          <PopoverContent className="max-w-[240px] p-16px text-14px font-medium text-gray-800">
+            <div>정말 이 태스크를 삭제할까요?</div>
+            <div className="mt-16px flex items-center justify-end gap-8px">
+              <button
+                onClick={closeDeletePopup}
+                className="rounded-md px-8px py-4px hover:bg-gray-100"
+              >
+                취소
+              </button>
+              <button
+                onClick={() => deleteTaskRequest(task.id)}
+                className="rounded-md px-8px py-4px hover:bg-gray-100"
+              >
+                확인
+              </button>
+            </div>
+          </PopoverContent>
+        </Popover>
+
         {dropdownOpened && (
           <div
             ref={dropdownRef}

dee4b3f645b2783c0ee89385502020c56546d045
ckm960411 <a01098956117@gmail.com>
2023-10-22 02:54:01 +0900

    add updateMilestone API

--- src/utils/services/milestone/index.ts
+++ src/utils/services/milestone/index.ts
@@ -1,4 +1,5 @@
-import { get, post } from "@/app/api/axios";
+import { get, patch, post } from "@/app/api/axios";
+import { UpdateMilestoneReqDto } from "@/utils/services/milestone/dto/update-milestone.req.dto";
 import { Milestone } from "@/utils/types/milestone";
 
 export const getAllMilestones = async () => {
@@ -10,5 +11,13 @@ export const createMilestone = async () => {
   const { data } = await post<
     Omit<Milestone, "tags" | "links" | "tasks" | "memos">
   >(`milestone`, "");
+  return data;
+};
+
+export const updateMilestone = async (
+  id: number,
+  updateMilestoneReqDto: UpdateMilestoneReqDto,
+) => {
+  const { data } = await patch(`milestone/${id}`, updateMilestoneReqDto);
   return data;
 };

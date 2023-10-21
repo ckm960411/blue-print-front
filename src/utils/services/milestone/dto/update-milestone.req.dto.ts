82e136be184eb59fce0d2bd9d36030075ad8cce6
ckm960411 <a01098956117@gmail.com>
2023-10-22 02:53:52 +0900

    add UpdateMilestoneReqDto type

---
+++ src/utils/services/milestone/dto/update-milestone.req.dto.ts
@@ -0,0 +1,13 @@
+import { Progress } from "@/utils/types";
+
+export interface UpdateMilestoneReqDto {
+  startAt?: Date;
+  endAt?: Date;
+  title?: string;
+  unicode?: string;
+  classification?: string;
+  priority?: 1 | 2 | 3 | 4 | 5;
+  progress: Progress;
+  isClosed?: boolean;
+  isBookmarked?: boolean;
+}

d32644cdd5667ee0e8dfc2def0e151f41fbab905
경민 <a01098956117@gmail.com>
2023-10-21 21:16:56 +0900

    add updateTag API

--- src/utils/services/tag/index.ts
+++ src/utils/services/tag/index.ts
@@ -1,6 +1,13 @@
-import { post } from "@/app/api/axios";
+import { patch, post } from "@/app/api/axios";
 import { CreateTagReqDto } from "@/utils/services/tag/dto/create-tag.req.dto";
 
 export const createTag = async (createTagReqDto: CreateTagReqDto) => {
   return await post(`tag`, createTagReqDto);
+};
+
+export const updateTag = async (
+  tagId: number,
+  updateTagReqDto: Partial<CreateTagReqDto>,
+) => {
+  return await patch(`tag/${tagId}`, updateTagReqDto);
 };

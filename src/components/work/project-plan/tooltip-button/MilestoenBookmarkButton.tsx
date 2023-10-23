29de2ed642fa84ac3d867712e8e0b9556ffd469c
ckm960411 <a01098956117@gmail.com>
2023-10-23 22:12:51 +0900

    milestone bookmark 구현

--- src/components/work/project-plan/tooltip-button/MilestoenBookmarkButton.tsx
+++ src/components/work/project-plan/tooltip-button/MilestoenBookmarkButton.tsx
@@ -1,4 +1,5 @@
 import IconButton from "@/components/components/IconButton";
+import { useUpdateMilestoneMutation } from "@/utils/hooks/react-query/useUpdateMilestoneMutation";
 import { Milestone } from "@/utils/types/milestone";
 import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
 import { Tooltip } from "@chakra-ui/react";
@@ -12,14 +13,21 @@ export default function MilestoenBookmarkButton({
   milestone,
   onClick,
 }: MilestoenBookmarkButtonProps) {
-  const { isBookmarked } = milestone;
+  const { id, isBookmarked } = milestone;
+
+  const { mutate: updateMilestoneRequest } = useUpdateMilestoneMutation(id);
+
   return (
     <Tooltip
       label="북마크"
       className="rounded-md bg-gray-100 px-8px py-6px text-12px text-gray-600"
     >
       <span>
-        <IconButton onClick={onClick}>
+        <IconButton
+          onClick={() =>
+            updateMilestoneRequest({ isBookmarked: !isBookmarked })
+          }
+        >
           {isBookmarked ? (
             <BsFillBookmarkFill className="text-red-500" />
           ) : (

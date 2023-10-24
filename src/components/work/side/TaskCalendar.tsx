7dfc82c9af02022e9b806fc62fcf891ea408e67f
ckm960411 <a01098956117@gmail.com>
2023-10-24 20:48:12 +0900

    달력 선택시 해당하는 일자의 태스크 팝업 출력

--- src/components/work/side/TaskCalendar.tsx
+++ src/components/work/side/TaskCalendar.tsx
@@ -1,55 +1,30 @@
-import { addTimeline } from "@/utils/common/task/calendar";
-import { getThisMonthTasks } from "@/utils/services/task";
-import { useQuery } from "@tanstack/react-query";
-import {
-  endOfDay,
-  getMonth,
-  getYear,
-  isWithinInterval,
-  startOfDay,
-} from "date-fns";
+import { Task } from "@/utils/types/task";
+import { endOfDay, isWithinInterval, startOfDay } from "date-fns";
 import { isNil } from "lodash";
 import { Calendar as PrimeCalendar } from "primereact/calendar";
 import { Nullable } from "primereact/ts-helpers";
-import React, {
-  Dispatch,
-  MutableRefObject,
-  SetStateAction,
-  useEffect,
-  useState,
-} from "react";
+import React, { Dispatch, MutableRefObject, SetStateAction } from "react";
 
 interface TaskCalendarProps {
-  setVisible: Dispatch<SetStateAction<boolean>>;
+  tasks: Task[];
+  date: Nullable<Date>;
+  setDate: Dispatch<SetStateAction<Nullable<Date>>>;
+  setMatchedTasks: Dispatch<SetStateAction<Task[]>>;
   calendar: MutableRefObject<PrimeCalendar | undefined>;
 }
-function TaskCalendar({ setVisible, calendar }: TaskCalendarProps) {
-  const [date, setDate] = useState<Nullable<Date>>(null);
-
-  const { data: tasks = [] } = useQuery(
-    ["get-this-month-tasks", date],
-    () =>
-      getThisMonthTasks({
-        year: date ? getYear(date) : undefined,
-        month: date ? getMonth(date) + 1 : undefined,
-      }),
-    {
-      onSuccess: (data) => addTimeline(data),
-      onError: console.error,
-    },
-  );
-
-  useEffect(() => {
-    date && tasks && addTimeline(tasks);
-  }, [date, tasks]);
-
+function TaskCalendar({
+  tasks,
+  date,
+  setDate,
+  setMatchedTasks,
+  calendar,
+}: TaskCalendarProps) {
   return (
     <div id="work-side-prime-calendar">
       <PrimeCalendar
         ref={calendar as any}
         value={date}
         onChange={({ originalEvent, value }) => {
-          console.log("originalEvent: ", originalEvent);
           if (isNil(value)) return;
           const foundTasks = tasks.filter((task) => {
             return (
@@ -61,8 +36,7 @@ function TaskCalendar({ setVisible, calendar }: TaskCalendarProps) {
               })
             );
           });
-
-          foundTasks.length > 0 && setVisible(true);
+          setMatchedTasks(foundTasks);
         }}
         inline
         showWeek
--- src/components/work/side/WorkSideCalendar.tsx
+++ src/components/work/side/WorkSideCalendar.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import TaskCalendar from "@/components/work/side/TaskCalendar";
+import { addTimeline } from "@/utils/common/task/calendar";
+import { getThisMonthTasks } from "@/utils/services/task";
+import { Task } from "@/utils/types/task";
+import { useQuery } from "@tanstack/react-query";
+import { getMonth, getYear } from "date-fns";
 import { ConfirmPopup } from "primereact/confirmpopup";
-import { useRef, useState } from "react";
+import { Nullable } from "primereact/ts-helpers";
+import { useEffect, useRef, useState } from "react";
 import { Calendar as PrimeCalendar } from "primereact/calendar";
 
 import "../../../css/work-side-prime-calendar.css";
@@ -11,27 +17,52 @@ interface WorkSideCalendarProps {}
 export default function WorkSideCalendar({}: WorkSideCalendarProps) {
   const calendar = useRef<PrimeCalendar>();
 
-  const [visible, setVisible] = useState(false);
+  const [date, setDate] = useState<Nullable<Date>>(null);
+  const [matchedTasks, setMatchedTasks] = useState<Task[]>([]);
+
+  const { data: tasks = [] } = useQuery(
+    ["get-this-month-tasks", date],
+    () =>
+      getThisMonthTasks({
+        year: date ? getYear(date) : undefined,
+        month: date ? getMonth(date) + 1 : undefined,
+      }),
+    {
+      onSuccess: (data) => addTimeline(data),
+      onError: console.error,
+    },
+  );
+
+  const handleClosePopup = () => setMatchedTasks([]);
+
+  useEffect(() => {
+    date && tasks && addTimeline(tasks);
+  }, [date, tasks]);
 
   return (
     <div>
-      <TaskCalendar calendar={calendar} setVisible={setVisible} />
+      <TaskCalendar
+        tasks={tasks}
+        date={date}
+        setDate={setDate}
+        calendar={calendar}
+        setMatchedTasks={setMatchedTasks}
+      />
       <ConfirmPopup
         target={calendar.current?.getElement()}
-        visible={visible}
-        onHide={() => setVisible(false)}
+        visible={matchedTasks.length > 0}
+        onHide={handleClosePopup}
         icon="pi pi-exclamation-triangle"
         className="w-full max-w-[367px]"
         message={
           <div className="w-full">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-            assumenda commodi mollitia nobis odit perspiciatis porro quae sint
-            unde vel. Aperiam error laborum mollitia rem! Illum optio quod quos
-            recusandae?
+            {matchedTasks.map((task) => (
+              <div key={task.id}>{task.id}</div>
+            ))}
           </div>
         }
         acceptLabel="닫기"
-        accept={() => setVisible(false)}
+        accept={handleClosePopup}
         acceptClassName="border border-gray-200 rounded-md py-4px px-8px"
         rejectClassName="hidden"
       />

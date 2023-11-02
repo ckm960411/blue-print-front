58f004881872ce57fb2fd5d7f99d82b96329db9e
ckm960411 <a01098956117@gmail.com>
2023-11-03 01:36:20 +0900

    milestone delete popup 수정

--- src/components/work/project-plan/tooltip-button/MilestoneTrashButton.tsx
+++ src/components/work/project-plan/tooltip-button/MilestoneTrashButton.tsx
@@ -1,15 +1,12 @@
+import React, { useRef, useState } from "react";
+import { BsTrash } from "react-icons/bs";
+import { useOnClickOutside } from "usehooks-ts";
+import { Tooltip } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 import { QueryKeys } from "@/utils/common/query-keys";
 import { deleteMilestone } from "@/utils/services/milestone";
 import { Milestone } from "@/utils/types/milestone";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import React, { useState } from "react";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-  Tooltip,
-} from "@chakra-ui/react";
-import { BsTrash } from "react-icons/bs";
 import IconButton from "@/components/components/IconButton";
 
 interface MilestoneTrashButtonProps {
@@ -18,8 +15,11 @@ interface MilestoneTrashButtonProps {
 export default function MilestoneTrashButton({
   milestone,
 }: MilestoneTrashButtonProps) {
+  const deletePopupRef = useRef<HTMLDivElement | null>(null);
   const queryClient = useQueryClient();
   const [openPopup, setOpenPopup] = useState(false);
+
+  useOnClickOutside(deletePopupRef, () => setOpenPopup(false));
 
   const { mutate: deleteMilestoneRequest } = useMutation(
     ["delete-milestone"],
@@ -44,15 +44,17 @@ export default function MilestoneTrashButton({
       label="휴지통"
       className="rounded-md bg-gray-100 px-8px py-6px text-12px text-gray-600"
     >
-      <span>
-        <Popover isOpen={openPopup} placement="bottom-end">
-          <PopoverTrigger>
-            <IconButton onClick={() => setOpenPopup(true)}>
-              <BsTrash />
-            </IconButton>
-          </PopoverTrigger>
-          <PopoverContent className="max-w-[240px] p-16px text-14px font-medium text-gray-800">
-            <div>정말 이 메모를 삭제할까요?</div>
+      <span className="relative">
+        <IconButton onClick={() => setOpenPopup(true)}>
+          <BsTrash />
+        </IconButton>
+
+        {openPopup && (
+          <div
+            ref={deletePopupRef}
+            className="absolute right-0 top-full w-240px rounded-10px bg-white p-16px text-14px font-medium text-gray-800 shadow-lg"
+          >
+            <div>정말 이 마일스톤을 삭제할까요?</div>
             <div className="mt-16px flex items-center justify-end gap-8px">
               <button
                 onClick={() => setOpenPopup(false)}
@@ -67,8 +69,8 @@ export default function MilestoneTrashButton({
                 확인
               </button>
             </div>
-          </PopoverContent>
-        </Popover>
+          </div>
+        )}
       </span>
     </Tooltip>
   );

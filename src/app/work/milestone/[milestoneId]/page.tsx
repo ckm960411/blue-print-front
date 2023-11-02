b7a1faadebbb20b6062ae9adea51dc587dc65962
ckm960411 <a01098956117@gmail.com>
2023-11-03 02:08:29 +0900

    GET Milestone in MilestonePage

--- src/app/work/milestone/[milestoneId]/page.tsx
+++ src/app/work/milestone/[milestoneId]/page.tsx
@@ -1,8 +1,34 @@
+"use client";
+
+import { MilestoneStatus } from "@/components/work/project-plan/ProjectPlanTab";
+import { QueryKeys } from "@/utils/common/query-keys";
+import { getMilestoneById } from "@/utils/services/milestone";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+
 interface MilestonePageProps {
   params: { milestoneId: number };
 }
 export default function MilestonePage({
   params: { milestoneId },
 }: MilestonePageProps) {
-  return <div>MilestonePage: {milestoneId}</div>;
+  const [status, setStatus] = useState<MilestoneStatus>("ALL");
+
+  const { data: milestone } = useQuery(
+    QueryKeys.getMilestoneById(milestoneId),
+    () => getMilestoneById(milestoneId),
+    { onError: console.error },
+  );
+
+  useEffect(() => {
+    getMilestoneById(milestoneId).then(console.log).catch(console.error);
+  }, []);
+
+  return (
+    <div className="h-full bg-gray-50 p-0px sm:p-16px">
+      <div className="mx-auto flex h-full max-w-screen-xl flex-col rounded-10px bg-white p-16px">
+        1
+      </div>
+    </div>
+  );
 }

65c9e3f219555d0ad927e69225a74a830360c08a
ckm960411 <a01098956117@gmail.com>
2023-11-03 02:22:48 +0900

    MilestonePage 마크업

--- src/app/work/milestone/[milestoneId]/page.tsx
+++ src/app/work/milestone/[milestoneId]/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { MilestoneStatus } from "@/components/work/project-plan/ProjectPlanTab";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
 import { QueryKeys } from "@/utils/common/query-keys";
 import { getMilestoneById } from "@/utils/services/milestone";
-import { useQuery } from "@tanstack/react-query";
-import React, { useEffect, useState } from "react";
+
+import MilestoneCardHeader from "@/components/work/project-plan/MilestoneCardHeader";
+import MilestoneCardSummary from "@/components/work/project-plan/MilestoneCardSummary";
+import { MilestoneStatus } from "@/components/work/project-plan/ProjectPlanTab";
+import MilestoneDrawerTabs from "@/components/work/project-plan/sidetab/MilestoneDrawerTabs";
+import MilestoenBookmarkButton from "@/components/work/project-plan/tooltip-button/MilestoenBookmarkButton";
+import MilestoneTrashButton from "@/components/work/project-plan/tooltip-button/MilestoneTrashButton";
 
 interface MilestonePageProps {
   params: { milestoneId: number };
@@ -24,10 +31,21 @@ export default function MilestonePage({
     getMilestoneById(milestoneId).then(console.log).catch(console.error);
   }, []);
 
+  if (!milestone) return <></>;
+
   return (
     <div className="h-full bg-gray-50 p-0px sm:p-16px">
-      <div className="mx-auto flex h-full max-w-screen-xl flex-col rounded-10px bg-white p-16px">
-        1
+      <div className="relative mx-auto flex h-full max-w-screen-xl flex-col gap-32px rounded-10px bg-white p-16px">
+        <div
+          className={`absolute right-16px top-20px flex items-center gap-8px`}
+        >
+          <MilestoneTrashButton milestone={milestone} />
+          <MilestoenBookmarkButton milestone={milestone} />
+        </div>
+
+        <MilestoneCardHeader milestone={milestone} toggleOpened isDetail />
+        <MilestoneCardSummary milestone={milestone} />
+        <MilestoneDrawerTabs milestone={milestone} />
       </div>
     </div>
   );

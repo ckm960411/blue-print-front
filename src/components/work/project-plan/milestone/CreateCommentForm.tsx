fa0d43629f11d9a6ec7334ea9626fe5d61653358
ckm960411 <a01098956117@gmail.com>
2023-11-04 05:38:48 +0900

    feature: createComment mutation

--- src/components/work/project-plan/milestone/CreateCommentForm.tsx
+++ src/components/work/project-plan/milestone/CreateCommentForm.tsx
@@ -1,3 +1,6 @@
+import { createComment } from "@/utils/services/comment";
+import { CreateCommentReqDto } from "@/utils/services/comment/dto/create-comment.req.dto";
+import { useMutation } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
@@ -11,6 +14,22 @@ export default function CreateCommentForm({
 }: CreateCommentFormProps) {
   const [content, setContent] = useState("");
 
+  const { mutate: createCommentRequest } = useMutation(
+    ["create-comment"],
+    (createCommentReqDto: CreateCommentReqDto) =>
+      createComment(createCommentReqDto),
+    {
+      onSuccess: console.log,
+      onError: console.error,
+    },
+  );
+
+  const handleConfirm = () => {
+    if (!content.trim()) return;
+
+    createCommentRequest({ content, milestoneId });
+  };
+
   return (
     <div className="flex flex-col gap-8px">
       <PlainEditor
@@ -19,10 +38,16 @@ export default function CreateCommentForm({
         height="80px"
       />
       <div className="flex items-center justify-end gap-8px">
-        <button className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100">
+        <button
+          onClick={() => setContent("")}
+          className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100"
+        >
           취소
         </button>
-        <button className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100">
+        <button
+          onClick={handleConfirm}
+          className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100"
+        >
           확인
         </button>
       </div>

df0f801ea8a605714bc015f5bc04d81825548cd2
ckm960411 <a01098956117@gmail.com>
2023-11-04 05:44:12 +0900

    feature: toggle showing form

--- src/components/work/project-plan/milestone/CreateCommentForm.tsx
+++ src/components/work/project-plan/milestone/CreateCommentForm.tsx
@@ -1,8 +1,11 @@
+import CreateButton from "@/components/work/components/CreateButton";
+import ToggleCheckOnly from "@/components/work/components/ToggleCheckOnly";
 import { createComment } from "@/utils/services/comment";
 import { CreateCommentReqDto } from "@/utils/services/comment/dto/create-comment.req.dto";
 import { useMutation } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { Toast } from "primereact/toast";
+import React, { useRef, useState } from "react";
 
 const PlainEditor = dynamic(() => import("../../components/PlainEditor"));
 
@@ -11,7 +14,10 @@ interface CreateCommentFormProps {
 }
 export default function CreateCommentForm({
   milestoneId,
-}: CreateCommentFormProps) {
+}: Readonly<CreateCommentFormProps>) {
+  const toast = useRef<Toast | null>(null);
+
+  const [showForm, setShowForm] = useState(false);
   const [content, setContent] = useState("");
 
   const { mutate: createCommentRequest } = useMutation(
@@ -19,7 +25,14 @@ export default function CreateCommentForm({
     (createCommentReqDto: CreateCommentReqDto) =>
       createComment(createCommentReqDto),
     {
-      onSuccess: console.log,
+      onSuccess: () => {
+        setShowForm(false);
+        toast.current?.show({
+          severity: "success",
+          summary: "댓글 작성 완료",
+          detail: "댓글 작성이 완료됐습니다.",
+        });
+      },
       onError: console.error,
     },
   );
@@ -30,27 +43,42 @@ export default function CreateCommentForm({
     createCommentRequest({ content, milestoneId });
   };
 
+  const handleCancel = () => {
+    setShowForm(false);
+    setContent("");
+  };
+
   return (
     <div className="flex flex-col gap-8px">
-      <PlainEditor
-        placeholder="댓글을 입력하세요"
-        onChange={(value) => setContent(value)}
-        height="80px"
-      />
-      <div className="flex items-center justify-end gap-8px">
-        <button
-          onClick={() => setContent("")}
-          className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100"
-        >
-          취소
-        </button>
-        <button
-          onClick={handleConfirm}
-          className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100"
-        >
-          확인
-        </button>
-      </div>
+      <Toast ref={toast} />
+      {showForm ? (
+        <>
+          <PlainEditor
+            placeholder="댓글을 입력하세요"
+            onChange={(value) => setContent(value)}
+            height="80px"
+          />
+          <div className="flex items-center justify-end gap-8px">
+            <button
+              onClick={handleCancel}
+              className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100"
+            >
+              취소
+            </button>
+            <button
+              onClick={handleConfirm}
+              className="rounded-md px-10px py-6px text-14px duration-200 hover:bg-gray-100"
+            >
+              확인
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="flex-between">
+          <ToggleCheckOnly checked={false} onClick={() => {}} />
+          <CreateButton onClick={() => setShowForm(true)} />
+        </div>
+      )}
     </div>
   );
 }

74d820f1e2781ee42afce54f514501177eb6534d
ckm960411 <a01098956117@gmail.com>
2023-11-05 00:44:08 +0900

    feature: getAllComments API

--- src/utils/services/comment/index.ts
+++ src/utils/services/comment/index.ts
@@ -1,9 +1,14 @@
-import { post } from "@/app/api/axios";
+import { get, post } from "@/app/api/axios";
 import { CreateCommentReqDto } from "@/utils/services/comment/dto/create-comment.req.dto";
 
 export const createComment = async (
   createCommentReqDto: CreateCommentReqDto,
 ) => {
   const { data } = await post<Comment>(`comment`, createCommentReqDto);
+  return data;
+};
+
+export const getAllComments = async (milestoneId: number) => {
+  const { data } = await get<Comment[]>(`comment`, { params: { milestoneId } });
   return data;
 };

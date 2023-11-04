1bbd72b325407bac400add85bb0c3a4546e2684e
ckm960411 <a01098956117@gmail.com>
2023-11-05 00:44:33 +0900

    feature: apply getAllComments

--- src/components/work/project-plan/milestone/CommentList.tsx
+++ src/components/work/project-plan/milestone/CommentList.tsx
@@ -1,4 +1,15 @@
-interface CommentListProps {}
-export default function CommentList({}: CommentListProps) {
+import { getAllComments } from "@/utils/services/comment";
+import { useQuery } from "@tanstack/react-query";
+
+interface CommentListProps {
+  milestoneId: number;
+}
+export default function CommentList({ milestoneId }: CommentListProps) {
+  const { data: comments } = useQuery(
+    ["get-all-comments", milestoneId],
+    () => getAllComments(milestoneId),
+    { onError: console.error },
+  );
+
   return <div>CommentList</div>;
 }

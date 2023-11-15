c0f182bc60d40184ab7d05aec6c4b0a454d301ed
ckm960411 <a01098956117@gmail.com>
2023-11-16 00:49:29 +0900

    💄 style: WorkPge 레이아웃 제거

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,31 +1,17 @@
 "use client";
 
-import { meState } from "@/utils/recoil/store";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
-import WorkPageHeader from "@/components/work/WorkPageHeader";
-import WorkPageContent from "@/components/work/WorkPageContent";
-import { useRecoilValue } from "recoil";
 
 export default function WorkPage() {
-  const me = useRecoilValue(meState);
   const router = useRouter();
 
   useEffect(() => {
-    if (!me) {
+    const token = window.localStorage.getItem("token");
+    if (!token) {
       router.push("/");
     }
-  }, [me]);
+  }, []);
 
-  return (
-    <div className="h-full bg-gray-50 p-0px sm:p-16px">
-      <div className="mx-auto flex h-full max-w-screen-xl flex-col rounded-10px bg-white">
-        <WorkPageHeader />
-        <div className="px-16px">
-          <hr />
-        </div>
-        <WorkPageContent />
-      </div>
-    </div>
-  );
+  return <div className="">WorkPage</div>;
 }

9034a50f7efedb1f5933f353ab47561f91095d85
ckm960411 <a01098956117@gmail.com>
2023-11-15 21:37:29 +0900

    💄 style: Sidebar 상단 마크업

--- src/components/common/SidebarV2.tsx
+++ src/components/common/SidebarV2.tsx
@@ -1,4 +1,19 @@
+import Link from "next/link";
+import React from "react";
+import { GoSidebarExpand } from "react-icons/go";
+
 interface SidebarV2Props {}
 export default function SidebarV2({}: SidebarV2Props) {
-  return <div>SidebarV2</div>;
+  return (
+    <div className="w-240px" style={{ backgroundColor: "#F6F8FF" }}>
+      <div className="flex-between h-56px px-16px">
+        <Link href="/work" className="text-22px font-semibold text-gray-800">
+          <span className="text-main">BluePrint</span>.Dev
+        </Link>
+        <button className="flex-center h-24px w-24px text-18px duration-200 hover:text-main">
+          <GoSidebarExpand />
+        </button>
+      </div>
+    </div>
+  );
 }

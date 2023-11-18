61f9af48aa1f3bf4fa87edc153cb4d2aea970d79
ckm960411 <a01098956117@gmail.com>
2023-11-18 18:33:18 +0900

    ♻️ Refactor: getMilestonesV2 호출위치 수정

--- src/components/work/milestone/MilestoneList.tsx
+++ src/components/work/milestone/MilestoneList.tsx
@@ -1,17 +1,63 @@
 import React, { Dispatch, SetStateAction } from "react";
-import { MilestoneWithContentCount } from "@/utils/types/milestone";
+import { useRecoilValue } from "recoil";
+import { useQuery } from "react-query";
+import { filter, map, pipe } from "lodash/fp";
+
+import { QueryKeys } from "@/utils/common/query-keys";
+import { projectState } from "@/utils/recoil/store";
+import { getAllMilestonesV2 } from "@/utils/services/milestone";
+import { Progress } from "@/utils/types";
+
 import MilestoneListCard from "@/components/work/milestone/MilestoneListCard";
+import { ProgressChecked } from "@/components/work/milestone/MilestoneTab";
 
 interface MilestoneListProps {
-  milestones: MilestoneWithContentCount[];
+  progressChecked: ProgressChecked;
   currentMilestoneId: number | null;
   setCurrentMilestoneId: Dispatch<SetStateAction<number | null>>;
 }
 export default function MilestoneList({
-  milestones,
+  progressChecked,
   currentMilestoneId,
   setCurrentMilestoneId,
 }: Readonly<MilestoneListProps>) {
+  const project = useRecoilValue(projectState);
+
+  const progresses = pipe(
+    filter(([key, value]: [Progress, boolean]) => value),
+    map(([key]) => key),
+  )(Object.entries(progressChecked));
+
+  const { data: milestoneListData } = useQuery(
+    QueryKeys.getAllMilestones(progresses, project?.id),
+    () =>
+      getAllMilestonesV2({
+        progresses,
+        projectId: project?.id,
+      }),
+    {
+      enabled: !!project?.id,
+      onSuccess: ({ items: milestones }) => {
+        if (milestones.length === 0) setCurrentMilestoneId(null);
+        if (currentMilestoneId) {
+          const hasCurrnetMilestone = milestones.some(
+            (milestone) => milestone.id === currentMilestoneId,
+          );
+          setCurrentMilestoneId(
+            hasCurrnetMilestone ? currentMilestoneId : milestones[0].id,
+          );
+        } else {
+          setCurrentMilestoneId(milestones[0].id);
+        }
+      },
+      onError: console.error,
+    },
+  );
+
+  if (!milestoneListData) return <></>;
+
+  const milestones = milestoneListData.items;
+
   return (
     <div className="flex flex-col gap-8px">
       {milestones.map((milestone) => (
--- src/components/work/milestone/MilestoneTabContent.tsx
+++ src/components/work/milestone/MilestoneTabContent.tsx
@@ -1,12 +1,4 @@
 import { useState } from "react";
-import { useQuery } from "react-query";
-import { useRecoilValue } from "recoil";
-import { filter, map, pipe } from "lodash/fp";
-
-import { QueryKeys } from "@/utils/common/query-keys";
-import { projectState } from "@/utils/recoil/store";
-import { getAllMilestonesV2 } from "@/utils/services/milestone";
-import { Progress } from "@/utils/types";
 
 import MilestoneDetail from "@/components/work/milestone/MilestoneDetail";
 import MilestoneList from "@/components/work/milestone/MilestoneList";
@@ -22,50 +14,13 @@ export default function MilestoneTabContent({
     null,
   );
 
-  const project = useRecoilValue(projectState);
-
-  const progresses = pipe(
-    filter(([key, value]: [Progress, boolean]) => value),
-    map(([key]) => key),
-  )(Object.entries(progressChecked));
-
-  const { data: milestoneListData } = useQuery(
-    QueryKeys.getAllMilestones(progresses, project?.id),
-    () =>
-      getAllMilestonesV2({
-        progresses,
-        projectId: project?.id,
-      }),
-    {
-      enabled: !!project?.id,
-      onSuccess: ({ items: milestones }) => {
-        if (milestones.length === 0) setCurrentMilestoneId(null);
-        if (currentMilestoneId) {
-          const hasCurrnetMilestone = milestones.some(
-            (milestone) => milestone.id === currentMilestoneId,
-          );
-          setCurrentMilestoneId(
-            hasCurrnetMilestone ? currentMilestoneId : milestones[0].id,
-          );
-        } else {
-          setCurrentMilestoneId(milestones[0].id);
-        }
-      },
-      onError: console.error,
-    },
-  );
-
-  if (!milestoneListData) return <></>;
-
-  const milestones = milestoneListData.items;
-
   return (
     <div className="mt-16px grid grid-cols-3 gap-12px">
       <div className="col-span-1 max-h-[720px] overflow-y-auto">
         <MilestoneList
-          milestones={milestones}
           currentMilestoneId={currentMilestoneId}
           setCurrentMilestoneId={setCurrentMilestoneId}
+          progressChecked={progressChecked}
         />
       </div>
       <div className="col-span-2 min-h-[720px] overflow-y-auto px-4px pb-4px">

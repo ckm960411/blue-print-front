3475991b97cefa605682c799df54077b824fa091
ckm960411 <a01098956117@gmail.com>
2023-11-21 01:09:57 +0900

    ✨ Add: 마일스톤 생성 기능

--- src/components/work/WorkTabMenuPlusButton.tsx
+++ src/components/work/WorkTabMenuPlusButton.tsx
@@ -1,5 +1,10 @@
 import { WorkTab } from "@/app/work/page";
+import { QueryKeys } from "@/utils/common/query-keys";
+import { projectState } from "@/utils/recoil/store";
+import { createMilestone } from "@/utils/services/milestone";
 import { FaPlus } from "react-icons/fa6";
+import { useMutation, useQueryClient } from "react-query";
+import { useRecoilValue } from "recoil";
 
 interface WorkTabMenuPlusButtonProps {
   workTab: WorkTab;
@@ -7,8 +12,33 @@ interface WorkTabMenuPlusButtonProps {
 export default function WorkTabMenuPlusButton({
   workTab,
 }: Readonly<WorkTabMenuPlusButtonProps>) {
+  const queryClient = useQueryClient();
+  const project = useRecoilValue(projectState);
+
+  const { mutate: createMilestoneRequest } = useMutation(
+    ["create-milestone"],
+    (projectId?: number) => createMilestone(projectId),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.getAllMilestones());
+      },
+      onError: console.error,
+    },
+  );
+
+  const handleClick = () => {
+    if (!project) return;
+
+    if (workTab === WorkTab.Milestone) {
+      createMilestoneRequest(project.id);
+    }
+  };
+
   return (
-    <button className="flex-center gap-4px rounded-md bg-gray-50 px-8px py-4px text-14px font-semibold duration-200 hover:bg-gray-100">
+    <button
+      onClick={handleClick}
+      className="flex-center gap-4px rounded-md bg-gray-50 px-8px py-4px text-14px font-semibold duration-200 hover:bg-gray-100"
+    >
       <FaPlus />
       <span>추가하기</span>
     </button>

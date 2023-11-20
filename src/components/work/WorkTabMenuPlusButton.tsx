d20f6c195942e176125826b4016da00e7610cd3e
ckm960411 <a01098956117@gmail.com>
2023-11-21 01:12:47 +0900

    ✨ Add: 태스크 생성기능 추가

--- src/components/work/WorkTabMenuPlusButton.tsx
+++ src/components/work/WorkTabMenuPlusButton.tsx
@@ -1,7 +1,10 @@
 import { WorkTab } from "@/app/work/page";
+import CreateUpdateTaskModal from "@/components/work/project-plan/CreateUpdateTaskModal";
 import { QueryKeys } from "@/utils/common/query-keys";
 import { projectState } from "@/utils/recoil/store";
 import { createMilestone } from "@/utils/services/milestone";
+import { useDisclosure } from "@chakra-ui/hooks";
+import React from "react";
 import { FaPlus } from "react-icons/fa6";
 import { useMutation, useQueryClient } from "react-query";
 import { useRecoilValue } from "recoil";
@@ -14,6 +17,11 @@ export default function WorkTabMenuPlusButton({
 }: Readonly<WorkTabMenuPlusButtonProps>) {
   const queryClient = useQueryClient();
   const project = useRecoilValue(projectState);
+  const {
+    isOpen: isOpenTaskPopup,
+    onOpen: openTaskPopup,
+    onClose: closeTaskPopup,
+  } = useDisclosure();
 
   const { mutate: createMilestoneRequest } = useMutation(
     ["create-milestone"],
@@ -32,15 +40,26 @@ export default function WorkTabMenuPlusButton({
     if (workTab === WorkTab.Milestone) {
       createMilestoneRequest(project.id);
     }
+
+    if (workTab === WorkTab.Task) {
+      openTaskPopup();
+    }
   };
 
   return (
-    <button
-      onClick={handleClick}
-      className="flex-center gap-4px rounded-md bg-gray-50 px-8px py-4px text-14px font-semibold duration-200 hover:bg-gray-100"
-    >
-      <FaPlus />
-      <span>추가하기</span>
-    </button>
+    <>
+      <button
+        onClick={handleClick}
+        className="flex-center gap-4px rounded-md bg-gray-50 px-8px py-4px text-14px font-semibold duration-200 hover:bg-gray-100"
+      >
+        <FaPlus />
+        <span>추가하기</span>
+      </button>
+
+      <CreateUpdateTaskModal
+        isOpen={isOpenTaskPopup}
+        onClose={closeTaskPopup}
+      />
+    </>
   );
 }

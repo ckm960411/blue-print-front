220e020f050a35b35faef92f6a729b755f02ece9
ckm960411 <a01098956117@gmail.com>
2023-11-21 01:51:18 +0900

    ✨ Feature: 마일스톤 북마크 기능

--- src/components/work/milestone/detail/MilestoneDetailNav.tsx
+++ src/components/work/milestone/detail/MilestoneDetailNav.tsx
@@ -1,9 +1,14 @@
-import IconButton from "@/components/components/IconButton";
-import { Milestone } from "@/utils/types/milestone";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { GrExpand } from "react-icons/gr";
+import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
+import { useQueryClient } from "react-query";
+
+import { QueryKeys } from "@/utils/common/query-keys";
+import { useUpdateMilestoneMutation } from "@/utils/hooks/react-query/useUpdateMilestoneMutation";
+import { Milestone } from "@/utils/types/milestone";
+import IconButton from "@/components/components/IconButton";
 
 interface MilestoneDetailNavProps {
   milestone: Milestone;
@@ -12,6 +17,18 @@ export default function MilestoneDetailNav({
   milestone,
 }: Readonly<MilestoneDetailNavProps>) {
   const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const { mutate: updateMilestoneRequest } = useUpdateMilestoneMutation(
+    milestone.id,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.getAllMilestones());
+        queryClient.invalidateQueries(QueryKeys.getMilestoneById(milestone.id));
+      },
+      onError: console.error,
+    },
+  );
 
   return (
     <div className="flex h-32px items-center justify-between px-16px">
@@ -21,9 +38,23 @@ export default function MilestoneDetailNav({
       >
         <GrExpand className="text-12px" />
       </IconButton>
-      <IconButton w={24}>
-        <BiDotsVerticalRounded className="text-18px" />
-      </IconButton>
+      <div className="flex-center gap-4px">
+        <IconButton
+          w={24}
+          onClick={() =>
+            updateMilestoneRequest({ isBookmarked: !milestone.isBookmarked })
+          }
+        >
+          {milestone.isBookmarked ? (
+            <BsFillBookmarkFill className="text-14px text-red-500" />
+          ) : (
+            <BsBookmark className="text-14px" />
+          )}
+        </IconButton>
+        <IconButton w={24}>
+          <BiDotsVerticalRounded className="text-18px" />
+        </IconButton>
+      </div>
     </div>
   );
 }

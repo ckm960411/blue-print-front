142166be19d54247c3cb3b275d793627c1461c8a
ckm960411 <a01098956117@gmail.com>
2023-11-21 01:59:58 +0900

    ✨ Feature: 마일스톤 삭제 구현

--- src/components/work/milestone/detail/MilestoneDetailNav.tsx
+++ src/components/work/milestone/detail/MilestoneDetailNav.tsx
@@ -1,14 +1,19 @@
-import { useRouter } from "next/navigation";
-import React from "react";
-import { BiDotsVerticalRounded } from "react-icons/bi";
-import { GrExpand } from "react-icons/gr";
-import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
-import { useQueryClient } from "react-query";
+import IconButton from "@/components/components/IconButton";
+import DeletePopup from "@/components/work/components/DeletePopup";
+import DropdownMenu from "@/components/work/components/task-card/DropdownMenu";
 
 import { QueryKeys } from "@/utils/common/query-keys";
 import { useUpdateMilestoneMutation } from "@/utils/hooks/react-query/useUpdateMilestoneMutation";
+import { deleteMilestone } from "@/utils/services/milestone";
 import { Milestone } from "@/utils/types/milestone";
-import IconButton from "@/components/components/IconButton";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { BsBookmark, BsFillBookmarkFill, BsTrash } from "react-icons/bs";
+import { GrExpand } from "react-icons/gr";
+import { useMutation, useQueryClient } from "react-query";
+import { useBoolean } from "usehooks-ts";
 
 interface MilestoneDetailNavProps {
   milestone: Milestone;
@@ -18,6 +23,17 @@ export default function MilestoneDetailNav({
 }: Readonly<MilestoneDetailNavProps>) {
   const router = useRouter();
   const queryClient = useQueryClient();
+
+  const {
+    value: dropdownOpened,
+    setTrue: openDropdown,
+    setFalse: closeDropdown,
+  } = useBoolean(false);
+  const {
+    isOpen: isDeletePopupOpen,
+    onOpen: openDeletePopup,
+    onClose: closeDeletePopup,
+  } = useDisclosure();
 
   const { mutate: updateMilestoneRequest } = useUpdateMilestoneMutation(
     milestone.id,
@@ -29,6 +45,26 @@ export default function MilestoneDetailNav({
       onError: console.error,
     },
   );
+
+  const { mutate: deleteMilestoneRequest } = useMutation(
+    ["delete-milestone"],
+    (id: number) => deleteMilestone(id),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.getAllMilestones());
+      },
+      onError: console.error,
+    },
+  );
+
+  const handleDelete = () => {
+    closeDeletePopup();
+    deleteMilestoneRequest(milestone.id);
+  };
+
+  const menus = [
+    { title: "삭제하기", icon: <BsTrash />, onClick: openDeletePopup },
+  ];
 
   return (
     <div className="flex h-32px items-center justify-between px-16px">
@@ -51,9 +87,23 @@ export default function MilestoneDetailNav({
             <BsBookmark className="text-14px" />
           )}
         </IconButton>
-        <IconButton w={24}>
-          <BiDotsVerticalRounded className="text-18px" />
-        </IconButton>
+        <div className="relative">
+          <IconButton w={24} onClick={openDropdown}>
+            <BiDotsVerticalRounded className="text-18px" />
+          </IconButton>
+          <DropdownMenu
+            open={dropdownOpened}
+            menus={menus}
+            onClose={closeDropdown}
+          />
+
+          <DeletePopup
+            open={isDeletePopupOpen}
+            title="정말 이 마일스톤을 삭제할까요?"
+            onClose={closeDeletePopup}
+            onComplete={handleDelete}
+          />
+        </div>
       </div>
     </div>
   );

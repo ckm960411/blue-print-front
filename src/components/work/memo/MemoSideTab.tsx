f037113a519ee50494eeb5600e5f5d664ba0813a
ckm960411 <a01098956117@gmail.com>
2023-11-20 22:40:52 +0900

    ♻️ Refactor: apply getAllMemos API

--- src/components/work/memo/MemoSideTab.tsx
+++ src/components/work/memo/MemoSideTab.tsx
@@ -1,14 +1,32 @@
+import { QueryKeys } from "@/utils/common/query-keys";
+import { projectState } from "@/utils/recoil/store";
+import { getAllMemos } from "@/utils/services/memo";
 import { FaRegBookmark } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa6";
 import IconButton from "@/components/components/IconButton";
+import { useQuery } from "react-query";
+import { useRecoilValue } from "recoil";
 
 interface MemoSideTabProps {}
 export default function MemoSideTab({}: MemoSideTabProps) {
+  const project = useRecoilValue(projectState);
+
+  const { data: memos } = useQuery(
+    QueryKeys.getAllMemos(project?.id),
+    () => getAllMemos({ projectId: project?.id, checked: false }),
+    {
+      enabled: !!project,
+      onError: console.error,
+    },
+  );
+
+  if (!memos) return <></>;
+
   return (
     <div className="min-h-screen w-240px flex-shrink-0 border-r border-gray-200 bg-gray-50">
       <div className="p-16px text-18px font-semibold">All Memos</div>
       <div className="flex-between p-16px">
-        <p className="text-12px text-gray-500">nn Memos</p>
+        <p className="text-12px text-gray-500">{memos.length} Memos</p>
         <div className="flex-center gap-4px">
           <IconButton
             w={24}
@@ -25,15 +43,20 @@ export default function MemoSideTab({}: MemoSideTabProps) {
         </div>
       </div>
       <div className="flex flex-col border-t border-gray-200">
-        <div className="border-b border-gray-200 bg-white p-8px">
-          <p className="truncate-2-lines text-14px leading-[140%] text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
-            exercitationem id perferendis quas ullam. Accusamus ad, explicabo
-            fugiat hic laborum nam quos veniam voluptatibus. Architecto
-            explicabo nam qui quidem soluta.
-          </p>
-          <div>1</div>
-        </div>
+        {memos.map((memo) => (
+          <div
+            key={memo.id}
+            className="border-b border-gray-200 bg-white p-8px"
+          >
+            <p className="truncate-2-lines text-14px leading-[140%] text-gray-600">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
+              exercitationem id perferendis quas ullam. Accusamus ad, explicabo
+              fugiat hic laborum nam quos veniam voluptatibus. Architecto
+              explicabo nam qui quidem soluta.
+            </p>
+            <div>1</div>
+          </div>
+        ))}
       </div>
     </div>
   );

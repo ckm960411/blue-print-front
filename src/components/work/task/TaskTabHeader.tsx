c6145189af0e9135deb0cde3115318696d173a64
ckm960411 <a01098956117@gmail.com>
2023-11-26 04:49:09 +0900

    🔥 Remove unused

--- src/components/work/task/TaskTabHeader.tsx
+++ src/components/work/task/TaskTabHeader.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 import { useRecoilValue } from "recoil";
-import { QueryKeys, taskKeys } from "@/utils/common/query-keys";
+import { taskKeys } from "@/utils/common/query-keys";
 import { projectState } from "@/utils/recoil/store";
 import { getAllTask } from "@/utils/services/task";
 
@@ -29,26 +29,19 @@ export default function TaskTabHeader() {
 
   return (
     <div className="p-16px">
-      <div className="flex">
-        <div className="flex grow flex-col gap-8px">
-          <p className="text-20px font-semibold text-gray-800">Daily Tasks</p>
-          <div className="flex w-full max-w-[420px] items-center gap-8px">
-            <span className="text-14px font-medium text-gray-600">진행도</span>
-            <div className="h-4px flex-shrink-0 grow rounded-full bg-gray-100">
-              <div
-                className="h-full rounded-full bg-green-400"
-                style={{ width: `${completedRatio}%` }}
-              />
-            </div>
-            <span className="text-14px font-medium text-gray-600">
-              {completedRatio}%
-            </span>
+      <div className="flex grow flex-col gap-8px">
+        <p className="text-20px font-semibold text-gray-800">Daily Tasks</p>
+        <div className="flex w-full max-w-[420px] items-center gap-8px">
+          <span className="text-14px font-medium text-gray-600">진행도</span>
+          <div className="h-4px flex-shrink-0 grow rounded-full bg-gray-100">
+            <div
+              className="h-full rounded-full bg-green-400"
+              style={{ width: `${completedRatio}%` }}
+            />
           </div>
-        </div>
-        <div className="flex-shrink-0">
-          <button className="rounded-md bg-gray-50 px-8px py-6px text-14px duration-200 hover:bg-gray-100">
-            할일 추가
-          </button>
+          <span className="text-14px font-medium text-gray-600">
+            {completedRatio}%
+          </span>
         </div>
       </div>
     </div>

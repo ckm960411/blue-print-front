bd05d84261141b83ec98b9b993990b6545f2ab18
ckm960411 <a01098956117@gmail.com>
2023-11-26 05:14:40 +0900

    ♻️ Apply ColorForm in MilestoneColorForm.tsx

--- src/components/work/milestone/MilestoneColorForm.tsx
+++ src/components/work/milestone/MilestoneColorForm.tsx
@@ -1,12 +1,10 @@
-import ColorPicker from "@/components/components/ColorPicker";
-import { ColorKey, Colors } from "@/utils/common/color";
+import React from "react";
+import { useQueryClient } from "react-query";
+
 import { QueryKeys } from "@/utils/common/query-keys";
 import { useUpdateMilestoneMutation } from "@/utils/hooks/react-query/work/milestone/useUpdateMilestoneMutation";
 import { Milestone } from "@/utils/types/milestone";
-import { useDisclosure } from "@chakra-ui/hooks";
-import { Popover, PopoverContent, PopoverTrigger } from "@chakra-ui/react";
-import { useQueryClient } from "react-query";
-import React, { useState } from "react";
+import ColorForm from "@/components/work/components/ColorForm";
 
 interface MilestoneColorFormProps {
   milestone: Milestone;
@@ -14,14 +12,7 @@ interface MilestoneColorFormProps {
 export default function MilestoneColorForm({
   milestone,
 }: MilestoneColorFormProps) {
-  const {
-    isOpen: isColorPickerOpen,
-    onOpen: openColorPicker,
-    onClose: closeColorPicker,
-  } = useDisclosure();
   const queryClient = useQueryClient();
-
-  const [color, setColor] = useState<ColorKey>(() => milestone.color ?? "gray");
 
   const { mutate: updateMilestoneRequest } = useUpdateMilestoneMutation(
     milestone.id,
@@ -32,37 +23,10 @@ export default function MilestoneColorForm({
     },
   );
 
-  const handleUpdate = () => {
-    updateMilestoneRequest({ color });
-    closeColorPicker();
-  };
-
   return (
-    <Popover isOpen={isColorPickerOpen} placement="bottom-start">
-      <PopoverTrigger>
-        <button
-          onClick={openColorPicker}
-          className="h-12px w-12px rounded-full"
-          style={{ backgroundColor: Colors[milestone.color][500] }}
-        />
-      </PopoverTrigger>
-      <PopoverContent className="max-w-[168px] p-16px">
-        <ColorPicker color={color} onClick={(color) => setColor(color)} />
-        <div className="mt-16px flex items-center justify-end gap-8px">
-          <button
-            onClick={closeColorPicker}
-            className="rounded-md px-8px py-4px text-14px hover:bg-gray-100"
-          >
-            취소
-          </button>
-          <button
-            onClick={handleUpdate}
-            className="rounded-md px-8px py-4px text-14px hover:bg-gray-100"
-          >
-            확인
-          </button>
-        </div>
-      </PopoverContent>
-    </Popover>
+    <ColorForm
+      initialColor={milestone.color}
+      onConfirm={(color) => updateMilestoneRequest({ color })}
+    />
   );
 }

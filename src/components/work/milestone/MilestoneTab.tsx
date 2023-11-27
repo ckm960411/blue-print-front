fdc9a745bc36f1c3a757921224464a63c2c64ca7
ckm960411 <a01098956117@gmail.com>
2023-11-28 01:21:48 +0900

    ♻️ Refactor: apply local progress checked state in MilestoneTab

--- src/components/work/milestone/MilestoneTab.tsx
+++ src/components/work/milestone/MilestoneTab.tsx
@@ -1,17 +1,36 @@
 import MilestoneTabCheckboxContainer from "@/components/work/milestone/MilestoneTabCheckboxContainer";
 import MilestoneTabContent from "@/components/work/milestone/MilestoneTabContent";
+import { WEB_STORAGE_KEY } from "@/utils/common/constant";
+import { useLocalStorage } from "@/utils/hooks/common/useLocalStorage";
 import { Progress } from "@/utils/types";
+import { useMountEffect } from "primereact/hooks";
 import { useState } from "react";
+import { useUpdateEffect } from "usehooks-ts";
 
 export type ProgressChecked = Record<Progress, boolean>;
+const initialProgressChecked = {
+  [Progress.ToDo]: true,
+  [Progress.InProgress]: false,
+  [Progress.Review]: false,
+  [Progress.Completed]: false,
+};
 
 export default function MilestoneTab() {
-  const [progressChecked, setProgressChecked] = useState<ProgressChecked>({
-    [Progress.ToDo]: true,
-    [Progress.InProgress]: false,
-    [Progress.Review]: false,
-    [Progress.Completed]: false,
+  const [localProgressChecked, setLocalProgressProgressChecked] =
+    useLocalStorage(
+      WEB_STORAGE_KEY.MILESTONE_PROGRESS_CHECKED,
+      initialProgressChecked,
+    );
+  const [progressChecked, setProgressChecked] = useState<ProgressChecked>(
+    initialProgressChecked,
+  );
+
+  useMountEffect(() => {
+    setProgressChecked(localProgressChecked);
   });
+  useUpdateEffect(() => {
+    setLocalProgressProgressChecked(progressChecked);
+  }, [progressChecked]);
 
   return (
     <div className="py-16px pl-16px pr-12px">
--- src/utils/common/constant/index.ts
+++ src/utils/common/constant/index.ts
@@ -1,4 +1,5 @@
 export const WEB_STORAGE_KEY = {
   TOKEN: "token",
   SIDEBAR_OPENED: "sidebar_opened",
+  MILESTONE_PROGRESS_CHECKED: "milestone_progress_checked",
 };

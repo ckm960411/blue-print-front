d37a4a606d18c52656175e9cf51c04042b902d37
ckm960411 <a01098956117@gmail.com>
2023-11-30 21:24:06 +0900

    ✨ Add WorkPage Calendar tab

--- src/components/work/WorkTabMenu.tsx
+++ src/components/work/WorkTabMenu.tsx
@@ -1,16 +1,18 @@
+import { Dispatch, SetStateAction } from "react";
+import { useRecoilValue } from "recoil";
+import { useQuery } from "react-query";
+import { isUndefined } from "lodash";
+
+import { FaRegStickyNote } from "react-icons/fa";
+import { GrTask } from "react-icons/gr";
+import { LuMilestone } from "react-icons/lu";
+import { MdCalendarToday } from "react-icons/md";
+
 import { QueryKeys } from "@/utils/common/query-keys";
 import { projectState } from "@/utils/recoil/store";
 import { getWorkCountByProjectId } from "@/utils/services/work";
 import { WorkTab } from "@/utils/types/work";
-import { isUndefined } from "lodash";
-import { Dispatch, SetStateAction } from "react";
-import { LuMilestone } from "react-icons/lu";
-import { GrTask } from "react-icons/gr";
-import { FaRegStickyNote } from "react-icons/fa";
-
 import WorkTabMenuPlusButton from "@/components/work/WorkTabMenuPlusButton";
-import { useQuery } from "react-query";
-import { useRecoilValue } from "recoil";
 
 interface WorkTabMenuProps {
   workTab: WorkTab;
@@ -26,21 +28,6 @@ export default function WorkTabMenu({
     setWorkTab(tab);
   };
 
-  const tabs = [
-    {
-      tab: WorkTab.Milestone,
-      Icon: LuMilestone,
-    },
-    {
-      tab: WorkTab.Task,
-      Icon: GrTask,
-    },
-    {
-      tab: WorkTab.Memo,
-      Icon: FaRegStickyNote,
-    },
-  ];
-
   const { data: workCount } = useQuery(
     QueryKeys.getWorkCount(project?.id),
     () => getWorkCountByProjectId(project?.id!),
@@ -49,11 +36,34 @@ export default function WorkTabMenu({
 
   if (isUndefined(workCount)) return <></>;
 
+  const tabs = [
+    {
+      tab: WorkTab.Milestone,
+      Icon: LuMilestone,
+      count: workCount.Milestone,
+    },
+    {
+      tab: WorkTab.Task,
+      Icon: GrTask,
+      count: workCount.Task,
+    },
+    {
+      tab: WorkTab.Memo,
+      Icon: FaRegStickyNote,
+      count: workCount.Memo,
+    },
+    {
+      tab: WorkTab.Calendar,
+      Icon: MdCalendarToday,
+      count: null,
+    },
+  ];
+
   return (
     <div className="sticky top-0 border-b border-gray-200 bg-white">
       <div className="flex-between rounded-md px-16px py-8px">
         <div className="inline-flex items-center gap-4px rounded-md bg-gray-100 px-8px py-4px">
-          {tabs.map(({ tab, Icon }) => {
+          {tabs.map(({ tab, Icon, count }) => {
             const isActive = tab === workTab;
             return (
               <button
@@ -65,11 +75,13 @@ export default function WorkTabMenu({
               >
                 <Icon />
                 <span>{tab}</span>
-                <span
-                  className={`inline-flex items-center justify-center rounded-md bg-gray-200 p-4px text-10px font-medium`}
-                >
-                  {workCount[tab]}
-                </span>
+                {count && (
+                  <span
+                    className={`inline-flex items-center justify-center rounded-md bg-gray-200 p-4px text-10px font-medium`}
+                  >
+                    {count}
+                  </span>
+                )}
               </button>
             );
           })}
--- src/utils/types/work/index.ts
+++ src/utils/types/work/index.ts
@@ -2,4 +2,5 @@ export enum WorkTab {
   Milestone = "Milestone",
   Task = "Task",
   Memo = "Memo",
+  Calendar = "Calendar",
 }

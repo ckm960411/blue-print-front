c4eae0296ee0507261a69dafca631dd1dd059691
ckm960411 <a01098956117@gmail.com>
2023-12-01 02:21:57 +0900

    ✨ 이번달 works 리스트 반환

--- src/components/work/calendar/CalendarTab.tsx
+++ src/components/work/calendar/CalendarTab.tsx
@@ -1,20 +1,94 @@
+import { projectState } from "@/utils/recoil/store";
+import { getThisMonthWorks } from "@/utils/services/work/[projectId]/calendar";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { LegacyRef, useRef } from "react";
+import { getMonth, getYear } from "date-fns";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { useQuery } from "react-query";
+import { useRecoilValue } from "recoil";
 
 export default function CalendarTab() {
   const calendarRef = useRef<FullCalendar>();
+  const project = useRecoilValue(projectState);
+
+  const [year, setYear] = useState(getYear(new Date()));
+  const [month, setMonth] = useState(getMonth(new Date()) - 1);
+
+  useQuery(
+    ["calendar", year, month],
+    () => {
+      if (!project?.id) return Promise.reject(new Error("no project Id"));
+      return getThisMonthWorks({ projectId: project.id, year, month });
+    },
+    {
+      enabled: !!project,
+      onSuccess: console.log,
+      onError: console.error,
+    },
+  );
+
+  const getYearAndWeek = (ref: MutableRefObject<FullCalendar | undefined>) => {
+    if (!ref.current) {
+      return {
+        year: getYear(new Date()),
+        month: getMonth(new Date()) - 1,
+      };
+    }
+    const date = ref.current.getApi().getDate();
+    const year = getYear(date);
+    const month = getMonth(date) + 1;
+    return { year, month };
+  };
+
+  useEffect(() => {
+    getThisMonthWorks({
+      projectId: 20,
+      year: 2023,
+      month: 11,
+    })
+      .then(console.log)
+      .catch(console.error);
+  }, []);
+
+  console.log(`year: ${year} / month: ${month}`);
 
   return (
     <div className="p-16px">
       <FullCalendar
-        ref={calendarRef as LegacyRef<FullCalendar>}
+        ref={calendarRef as MutableRefObject<FullCalendar>}
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         fixedWeekCount={false}
         titleFormat={{ year: "numeric", month: "short" }}
         weekNumbers={true}
+        customButtons={{
+          prevButton: {
+            text: "prev",
+            click: () => {
+              if (!calendarRef.current) return;
+              calendarRef.current.getApi().prev();
+              const { year, month } = getYearAndWeek(calendarRef);
+              setYear(year);
+              setMonth(month);
+            },
+          },
+          nextButton: {
+            text: "next",
+            click: () => {
+              if (!calendarRef.current) return;
+              calendarRef.current?.getApi().next();
+              const { year, month } = getYearAndWeek(calendarRef);
+
+              setYear(year);
+              setMonth(month);
+            },
+          },
+        }}
+        headerToolbar={{
+          left: "title",
+          right: "prevButton,nextButton",
+        }}
         events={[
           {
             id: "1",

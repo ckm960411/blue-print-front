b8d1c17b49d0ba36e6e6857663fbeb93abd9e5c2
ckm960411 <a01098956117@gmail.com>
2023-11-30 21:35:14 +0900

    ✨ Apply multiMonthPlugin view

--- src/components/work/calendar/CalendarTab.tsx
+++ src/components/work/calendar/CalendarTab.tsx
@@ -1,10 +1,15 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import multiMonthPlugin from "@fullcalendar/multimonth";
 
 export default function CalendarTab() {
   return (
     <div className="p-16px">
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        plugins={[dayGridPlugin, multiMonthPlugin]}
+        initialView="multiMonthYear"
+        multiMonthMaxColumns={1}
+      />
     </div>
   );
 }

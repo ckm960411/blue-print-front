e878f3fb6a7bd9086710dddec1800469146e30f8
ckm960411 <a01098956117@gmail.com>
2023-12-01 02:26:41 +0900

    ✨ Add CalendarWork return type

--- src/utils/services/work/[projectId]/calendar/index.ts
+++ src/utils/services/work/[projectId]/calendar/index.ts
@@ -1,14 +1,41 @@
 import { get } from "@/app/api/axios";
+import { ColorKey } from "@/utils/common/color";
+import { DateTime, Progress } from "@/utils/types";
 
 export interface GetThisMonthWorksReqDto {
   projectId: number;
   year?: number;
   month?: number;
 }
+
+export interface CalendarMilestone {
+  milestoneId: number;
+  taskId: null;
+  title: string;
+  unicode: string;
+  progress: Progress;
+  color: ColorKey;
+  startAt: DateTime;
+  endAt: DateTime;
+}
+export interface CalendarTask {
+  milestoneId: null;
+  taskId: number;
+  title: string;
+  unicode: null;
+  progress: Progress;
+  color: ColorKey;
+  startAt: DateTime;
+  endAt: DateTime;
+}
+export type CalendarWork = CalendarMilestone | CalendarTask;
+
 export const getThisMonthWorks = async ({
   projectId,
   ...params
 }: GetThisMonthWorksReqDto) => {
-  const { data } = await get(`work/${projectId}/calendar`, { params });
+  const { data } = await get<CalendarWork[]>(`work/${projectId}/calendar`, {
+    params,
+  });
   return data;
 };

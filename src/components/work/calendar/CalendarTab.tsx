302f661090c750b331275a03d79ac0ba52159680
ckm960411 <a01098956117@gmail.com>
2023-11-30 21:31:33 +0900

    ✨ Add FullCalendar component

--- src/components/work/calendar/CalendarTab.tsx
+++ src/components/work/calendar/CalendarTab.tsx
@@ -1,3 +1,10 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
 export default function CalendarTab() {
-  return <div>CalendarTab</div>;
+  return (
+    <div className="p-16px">
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+    </div>
+  );
 }

9d9bfb4aef26fa54b652ab2375aa3bde87ae3d6a
ckm960411 <a01098956117@gmail.com>
2023-12-03 18:07:35 +0900

    ✨ Feature: weekDays button onClick

--- src/components/health/HealthWeekDays.tsx
+++ src/components/health/HealthWeekDays.tsx
@@ -1,4 +1,10 @@
-import { eachDayOfInterval, endOfWeek, format, startOfWeek } from "date-fns";
+import {
+  addDays,
+  eachDayOfInterval,
+  endOfWeek,
+  format,
+  startOfWeek,
+} from "date-fns";
 import { Dispatch, SetStateAction } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
@@ -16,11 +22,12 @@ export default function HealthWeekDays({
     const end = endOfWeek(now, { weekStartsOn: 1 }); // 일요일까지
 
     const interval = { start, end };
-    const days = eachDayOfInterval(interval);
+    const dates = eachDayOfInterval(interval);
 
-    return days.map((day) => ({
-      date: format(day, "dd"),
-      day: format(day, "eeee").slice(0, 3),
+    return dates.map((date) => ({
+      date,
+      dateString: format(date, "dd"),
+      day: format(date, "eeee").slice(0, 3),
     }));
   })();
 
@@ -33,24 +40,33 @@ export default function HealthWeekDays({
       </p>
       <div className="flex items-center gap-24px">
         <div className="flex items-center gap-16px">
-          <button className="flex-center bg-lightblue h-32px w-32px rounded-full text-14px duration-200 hover:bg-blue-100 hover:text-main">
+          <button
+            onClick={() => setThisDate(addDays(thisDate, -7))}
+            className="flex-center bg-lightblue h-32px w-32px rounded-full text-14px duration-200 hover:bg-blue-100 hover:text-main"
+          >
             <FaChevronLeft />
           </button>
-          <button className="flex-center bg-lightblue h-32px w-32px rounded-full text-14px duration-200 hover:bg-blue-100 hover:text-main">
+          <button
+            onClick={() => setThisDate(addDays(thisDate, 7))}
+            className="flex-center bg-lightblue h-32px w-32px rounded-full text-14px duration-200 hover:bg-blue-100 hover:text-main"
+          >
             <FaChevronRight />
           </button>
         </div>
         <div className="flex items-center gap-8px">
-          {weekDays.map(({ date, day }) => {
-            const isToday = date === format(thisDate, "dd");
+          {weekDays.map(({ date, dateString, day }) => {
+            const isToday = dateString === format(thisDate, "dd");
             return (
               <button
                 key={`${date}-${day}`}
-                className={`flex-center h-56px w-48px flex-col gap-4px rounded-md text-14px font-medium ${
-                  isToday ? "bg-main text-white" : "bg-lightblue text-gray-800"
+                onClick={() => setThisDate(date)}
+                className={`flex-center h-56px w-48px flex-col gap-4px rounded-md text-14px font-medium duration-200 ${
+                  isToday
+                    ? "bg-main text-white"
+                    : "bg-lightblue text-gray-800 hover:bg-blue-100"
                 }`}
               >
-                <span>{date}</span>
+                <span>{dateString}</span>
                 <span>{day}</span>
               </button>
             );

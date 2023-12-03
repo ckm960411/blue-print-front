4c63197752e444f769cfdb002ff15322f6d0050c
ckm960411 <a01098956117@gmail.com>
2023-12-03 16:48:11 +0900

    ✨ Add HealthPageHeader

--- src/app/health/page.tsx
+++ src/app/health/page.tsx
@@ -1,3 +1,10 @@
+import HealthPageHeader from "@/components/health/HealthPageHeader";
+
 export default function page() {
-  return <div>Health Page</div>;
+  return (
+    <section>
+      <HealthPageHeader />
+      <div className="mx-auto max-w-[1280px] p-16px">Health Page</div>
+    </section>
+  );
 }
---
+++ src/components/health/HealthPageHeader.tsx
@@ -0,0 +1,7 @@
+export default function HealthPageHeader() {
+  return (
+    <div className="border-b border-gray-200 px-24px py-16px">
+      <h1 className="text-28px font-bold">Health</h1>
+    </div>
+  );
+}

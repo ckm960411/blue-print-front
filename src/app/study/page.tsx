6b38c32a32562240ce385f76859071bebce7c240
ckm960411 <a01098956117@gmail.com>
2023-12-03 14:49:50 +0900

    💄 Style: Page padding top

--- src/app/study/page.tsx
+++ src/app/study/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 };
 export default function StudyPage() {
   return (
-    <section className="px-16px pb-72px">
+    <section className="px-16px pb-72px pt-16px">
       <section className="relative overflow-hidden rounded-10px bg-main ">
         <Image
           src={dashboardImage.src}

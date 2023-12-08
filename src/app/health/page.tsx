9a0df29835c1adb5174ee558a730f625accdb514
ckm960411 <a01098956117@gmail.com>
2023-12-09 02:01:43 +0900

    ✨ Add HealthMonthly

--- src/app/health/page.tsx
+++ src/app/health/page.tsx
@@ -2,12 +2,14 @@
 
 import HealthDashboard from "@/components/health/HealthDashboard";
 import HealthDaySelect from "@/components/health/HealthDaySelect";
+import HealthMonthly from "@/components/health/HealthMonthly";
 
 export default function HealthPage() {
   return (
     <section>
       <HealthDaySelect />
       <HealthDashboard />
+      <HealthMonthly />
     </section>
   );
 }
---
+++ src/components/health/HealthMonthly.tsx
@@ -0,0 +1,24 @@
+interface HealthMonthlyProps {}
+export default function HealthMonthly({}: Readonly<HealthMonthlyProps>) {
+  return (
+    <div className="p-16px">
+      <div className="rounded-md p-16px shadow-md">
+        <p className="text-14px font-bold text-main">🏃이번 달 운동들</p>
+        <div className="flex items-start gap-16px">
+          <div className="relative">
+            <span className="text-[60px]">🏅</span>
+            <span className="absolute inset-x-0 -bottom-4px text-center text-14px text-gray-600">
+              <span className="text-16px font-bold text-main">n</span> / n
+            </span>
+          </div>
+          <div className="flex flex-col gap-8px pt-12px text-14px">
+            <p className="truncate-1-lines">턱걸이 n회</p>
+            <p className="truncate-1-lines">푸시업 n회</p>
+            <p className="truncate-1-lines">달리기 n회</p>
+            <p className="truncate-1-lines">덤벨 n회</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

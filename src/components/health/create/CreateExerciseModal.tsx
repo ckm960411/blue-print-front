b54408afc983dac9921f4ef450d27c57270631ff
ckm960411 <a01098956117@gmail.com>
2023-12-10 02:13:34 +0900

    ♻️ Refactor: add CreateExerciseSelectType

--- src/components/health/create/CreateExerciseModal.tsx
+++ src/components/health/create/CreateExerciseModal.tsx
@@ -1,8 +1,5 @@
-import { Colors } from "@/utils/common/color";
-import { useAllExerciseTypeQuery } from "@/utils/hooks/react-query/health/useAllExerciseTypeQuery";
-import { ExerciseType } from "@/utils/types/health";
-import { Button } from "@chakra-ui/button";
-import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import {
   Modal,
   ModalBody,
@@ -11,9 +8,6 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/modal";
-import { useState } from "react";
-import { useMediaQuery } from "react-responsive";
-import { IoChevronDownSharp } from "react-icons/io5";
 import {
   NumberDecrementStepper,
   NumberIncrementStepper,
@@ -21,6 +15,9 @@ import {
   NumberInputField,
   NumberInputStepper,
 } from "@chakra-ui/react";
+
+import CreateExerciseSelectType from "@/components/health/create/CreateExerciseSelectType";
+import { ExerciseType } from "@/utils/types/health";
 
 interface CreateExerciseModalProps {
   isOpen: boolean;
@@ -34,7 +31,6 @@ export default function CreateExerciseModal({
   const [count, setCount] = useState(0);
 
   const UNDER_768PX = useMediaQuery({ query: "(max-width: 767px)" });
-  const exerciseTypes = useAllExerciseTypeQuery();
 
   return (
     <Modal onClose={onClose} size={UNDER_768PX ? "full" : "lg"} isOpen={isOpen}>
@@ -46,31 +42,10 @@ export default function CreateExerciseModal({
           <div className="flex flex-col gap-16px">
             <p className="text-16px font-medium">어떤 운동을 했나요?</p>
             <div className="flex items-center gap-16px text-14px">
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rightIcon={<IoChevronDownSharp />}
-                  borderRadius="md"
-                  borderWidth="1px"
-                  _expanded={{ bg: "#fff" }}
-                  className="truncate-1-lines w-144px px-8px py-4px text-left text-14px"
-                  style={{ display: "flex" }}
-                >
-                  {type?.name ?? "선택"}
-                </MenuButton>
-                <MenuList>
-                  {exerciseTypes.map((type) => (
-                    <MenuItem
-                      key={type.id}
-                      onClick={() => setType(type)}
-                      className="py-4px text-left text-14px"
-                      _hover={{ bg: Colors.gray[50] }}
-                    >
-                      {type.name}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </Menu>
+              <CreateExerciseSelectType
+                type={type}
+                onSelect={(type) => setType(type)}
+              />
               {type && (
                 <div className="flex grow items-center gap-8px">
                   <NumberInput

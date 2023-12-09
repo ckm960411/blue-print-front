462188dbc4850f222b93242f3b88c27dc3e59af9
ckm960411 <a01098956117@gmail.com>
2023-12-09 20:03:08 +0900

    ♻️ Refactor: useMonthExercisesQuery options type

--- src/utils/hooks/react-query/health/useMonthExercisesQuery.tsx
+++ src/utils/hooks/react-query/health/useMonthExercisesQuery.tsx
@@ -2,21 +2,23 @@ import { exerciseKeys } from "@/utils/common/query-keys";
 import { useMe } from "@/utils/common/user/useMe";
 import { getMonthExercises } from "@/utils/services/health";
 import { Exercise } from "@/utils/types/health";
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 
-export const useMonthExercisesQuery = (options?: {
-  year?: number;
-  month?: number;
-  onSuccess?: (exercises: Exercise[]) => void;
-}) => {
+export const useMonthExercisesQuery = <T = Exercise[],>(
+  options?: {
+    year?: number;
+    month?: number;
+  } & UseQueryOptions<Exercise[], unknown, T>,
+) => {
   const me = useMe();
 
-  return useQuery(
+  return useQuery<Exercise[], unknown, T>(
     exerciseKeys.list(me?.id),
     () => getMonthExercises({ year: options?.year, month: options?.month }),
     {
       onSuccess: options?.onSuccess,
       onError: console.error,
+      select: options?.select,
     },
   );
 };

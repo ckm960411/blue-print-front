c337286a06c8000c6248f0968d68173d1fd40134
ckm960411 <a01098956117@gmail.com>
2023-12-09 17:13:48 +0900

    ✨ Add getWeeklyExerciseChecked API

--- src/utils/services/health/index.ts
+++ src/utils/services/health/index.ts
@@ -1,10 +1,26 @@
 import { get } from "@/app/api/axios";
 import { Exercise } from "@/utils/types/health";
+import { format, startOfDay } from "date-fns/fp";
+import { flow } from "lodash/fp";
 
 export const getMonthExercises = async (params?: {
   year?: number;
   month?: number;
 }) => {
   const { data } = await get<Exercise[]>(`health/exercises`, { params });
+  return data;
+};
+
+export type GetWeeklyExerciseCheckedResDto = Record<
+  0 | 1 | 2 | 3 | 4 | 5 | 6,
+  boolean
+>;
+export const getWeeklyExerciseChecked = async () => {
+  const today = flow(startOfDay, format("yyyy-MM-dd"))(new Date());
+
+  const { data } = await get<GetWeeklyExerciseCheckedResDto>(
+    `health/week/checked`,
+    { params: { today } },
+  );
   return data;
 };

20b8a2dc70232413958296ba066cafdf5e8fcfe1
ckm960411 <a01098956117@gmail.com>
2023-12-23 15:49:55 +0900

    ✨ Create CategoryEmojiSelect

---
+++ src/components/money/budget/setting/CategoryEmojiSelect.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Unicode from "@/components/components/Unicode";
+
+interface CategoryEmojiSelectProps {}
+export default function CategoryEmojiSelect({}: Readonly<CategoryEmojiSelectProps>) {
+  return (
+    <div>
+      <Unicode value={"1f4b5"} onClick={() => {}} className="text-18px" />
+    </div>
+  );
+}

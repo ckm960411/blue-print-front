1b1cc674cec2efa5f2638474ad2fc2cdd34801c8
ckm960411 <a01098956117@gmail.com>
2023-12-23 16:01:15 +0900

    ✨ Add BudgetCategoryCard in CategorySettingPage

--- src/app/money/category/setting/page.tsx
+++ src/app/money/category/setting/page.tsx
@@ -1,4 +1,6 @@
+import BudgetCategoryCard from "@/components/money/budget/setting/BudgetCategoryCard";
 import BudgetCategorySettingCloseButton from "@/components/money/budget/setting/BudgetCategorySettingCloseButton";
+import React from "react";
 
 export default function CategorySettingPage() {
   return (
@@ -6,6 +8,9 @@ export default function CategorySettingPage() {
       <div className="flex-between border-b border-gray-200 p-16px">
         <p className="text-18px font-bold">카테고리 설정</p>
         <BudgetCategorySettingCloseButton />
+      </div>
+      <div className="flex flex-col gap-8px p-16px">
+        <BudgetCategoryCard />
       </div>
     </div>
   );

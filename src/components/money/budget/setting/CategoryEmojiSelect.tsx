54207e8e2a65b549b32f12b1bb9a472cb14a192f
ckm960411 <a01098956117@gmail.com>
2023-12-23 15:55:17 +0900

    ✨ Add PickerWrapper in CategoryEmojiSelect.tsx

--- src/components/money/budget/setting/CategoryEmojiSelect.tsx
+++ src/components/money/budget/setting/CategoryEmojiSelect.tsx
@@ -1,12 +1,37 @@
 "use client";
 
-import Unicode from "@/components/components/Unicode";
+import PickerWrapper from "@/components/components/PickerWrapper";
+import Unicode, { EmojiType } from "@/components/components/Unicode";
+import { useClickOutside } from "primereact/hooks";
+import React, { useRef, useState } from "react";
 
-interface CategoryEmojiSelectProps {}
-export default function CategoryEmojiSelect({}: Readonly<CategoryEmojiSelectProps>) {
+interface CategoryEmojiSelectProps {
+  unicode: string;
+}
+export default function CategoryEmojiSelect({
+  unicode,
+}: Readonly<CategoryEmojiSelectProps>) {
+  const pickerWrapperRef = useRef<HTMLDivElement | null>(null);
+  const [showPicker, setShowPicker] = useState(false);
+
+  useClickOutside(pickerWrapperRef, () => setShowPicker(false));
+
+  const handleOpen = () => setShowPicker(true);
+  const handleClose = () => setShowPicker(false);
+
+  const handleEmojiSelect = ({ unified: unicode }: EmojiType) => {
+    console.log("unicode", unicode);
+    handleClose();
+  };
+
   return (
-    <div>
-      <Unicode value={"1f4b5"} onClick={() => {}} className="text-18px" />
+    <div className="relative">
+      <Unicode value={unicode} onClick={handleOpen} className="text-18px" />
+      {showPicker && (
+        <div ref={pickerWrapperRef} className="absolute left-0 top-0 z-10">
+          <PickerWrapper onEmojiSelect={handleEmojiSelect} />
+        </div>
+      )}
     </div>
   );
 }

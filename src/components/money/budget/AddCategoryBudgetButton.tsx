437d7efeb4c8e6190a767db9ca45c410a0e4f243
ckm960411 <a01098956117@gmail.com>
2023-12-23 15:22:28 +0900

    ✨ Add AddCategoryBudgetButton

---
+++ src/components/money/budget/AddCategoryBudgetButton.tsx
@@ -0,0 +1,7 @@
+export default function AddCategoryBudgetButton() {
+  return (
+    <button className="w-full rounded-10px bg-main p-16px text-16px font-bold text-white shadow-lg">
+      예산 추가하기
+    </button>
+  );
+}
--- src/components/money/budget/CategoryBudget.tsx
+++ src/components/money/budget/CategoryBudget.tsx
@@ -1,22 +1,13 @@
+import AddCategoryBudgetButton from "@/components/money/budget/AddCategoryBudgetButton";
 import CategoryBudgetContainer from "@/components/money/budget/CategoryBudgetContainer";
 import CategoryBudgetHeader from "@/components/money/budget/CategoryBudgetHeader";
-import SpaceY from "@/components/common/SpaceY";
 
 export default function CategoryBudget() {
   return (
-    <div>
+    <div className="flex flex-col gap-32px">
       <CategoryBudgetHeader />
-
-      <SpaceY height={32} />
-
       <CategoryBudgetContainer />
-
-      <SpaceY height={32} />
-      <div>
-        <button className="w-full rounded-10px bg-main p-16px text-16px font-bold text-white shadow-lg">
-          예산 추가하기
-        </button>
-      </div>
+      <AddCategoryBudgetButton />
     </div>
   );
 }

b3d248490db12df7c96b56a6c7430d93f50ecb0d
ckm960411 <a01098956117@gmail.com>
2023-12-26 20:57:07 +0900

    💄 Style: 예산이 아직 없는 경우 마크업

--- src/components/money/budget/monthly/MonthlyBudget.tsx
+++ src/components/money/budget/monthly/MonthlyBudget.tsx
@@ -25,7 +25,20 @@ export default function MonthlyBudget() {
   }
 
   if (!monthlyBudget) {
-    return <div>no monthlyBudget</div>;
+    return (
+      <div className="flex-center flex-col gap-16px py-40px text-center">
+        <p className="text-50px">💸</p>
+        <p className="text-18px font-bold leading-[140%]">
+          이번 달 예산을 아직 책정하지 않았어요!
+        </p>
+        <p className="whitespace-pre-wrap font-medium text-gray-700">
+          돈이 줄줄 새기 전에 현명한 소비를 위해{"\n"}예산을 측정해봅시다!
+        </p>
+        <button className="w-full rounded-10px bg-main p-16px text-18px font-bold text-white shadow-lg">
+          예산 책정하기
+        </button>
+      </div>
+    );
   }
 
   // TODO: 카테고리별 예산 총액

13a69b16c8bc0c687456db7c2606144697ba96e3
ckm960411 <a01098956117@gmail.com>
2023-12-27 00:11:32 +0900

    ✨ Add BudgetCateogry Dropdown Menu

--- src/components/money/budget/setting/CreateCategoryBudgetModal.tsx
+++ src/components/money/budget/setting/CreateCategoryBudgetModal.tsx
@@ -1,3 +1,9 @@
+import { Colors } from "@/utils/common/color";
+import { useMe } from "@/utils/common/user/useMe";
+import { getAllBudgetCategories } from "@/utils/services/money";
+import { BudgetCategory } from "@/utils/types/money";
+import { Button } from "@chakra-ui/button";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import {
   Modal,
   ModalBody,
@@ -7,8 +13,9 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/modal";
-import { getMonth } from "date-fns";
-import React from "react";
+import React, { useState } from "react";
+import { IoChevronDownSharp } from "react-icons/io5";
+import { useQuery } from "react-query";
 
 interface SettingCategoryBudgetModalProps {
   isOpen: boolean;
@@ -18,6 +25,17 @@ export default function CreateCategoryBudgetModal({
   isOpen,
   onClose,
 }: Readonly<SettingCategoryBudgetModalProps>) {
+  const me = useMe();
+  const [currentCategory, setCurrentCategory] = useState<BudgetCategory | null>(
+    null,
+  );
+
+  const { data: categories = [] } = useQuery(
+    ["get-all-budget-categories", me?.id],
+    getAllBudgetCategories,
+    { onError: console.error },
+  );
+
   const handleClose = () => {
     onClose();
   };
@@ -37,7 +55,36 @@ export default function CreateCategoryBudgetModal({
         <ModalHeader>카테고리별 예산 책정하기</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <div>카테고리별 예산 책정하기</div>
+          <div className="flex flex-col gap-16px">
+            <p className="text-18px font-bold">생성할 카테고리 선택</p>
+            <div>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rightIcon={<IoChevronDownSharp />}
+                  borderRadius="md"
+                  borderWidth="1px"
+                  _expanded={{ bg: "#fff" }}
+                  className="truncate-1-lines w-144px px-8px py-4px text-left text-14px"
+                  style={{ display: "flex" }}
+                >
+                  {currentCategory?.name ?? "선택"}
+                </MenuButton>
+                <MenuList>
+                  {categories.map((category) => (
+                    <MenuItem
+                      key={category.id}
+                      onClick={() => setCurrentCategory(category)}
+                      className="py-8px text-left text-14px"
+                      _hover={{ bg: Colors.gray[50] }}
+                    >
+                      {category.name}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </Menu>
+            </div>
+          </div>
         </ModalBody>
         <ModalFooter>
           <div className="flex items-center gap-8px">

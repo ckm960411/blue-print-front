2328aa695ddcb2bd7d1bef94e1ab9f3b97ff097f
ckm960411 <a01098956117@gmail.com>
2023-12-28 00:09:23 +0900

    ✨ 카테고리 예산 생성기능 구현

--- src/components/money/budget/setting/CreateCategoryBudgetModal.tsx
+++ src/components/money/budget/setting/CreateCategoryBudgetModal.tsx
@@ -1,6 +1,10 @@
 import CreateBudgetCategoryDropdown from "@/components/money/budget/setting/CreateBudgetCategoryDropdown";
 import CreateBudgetCategorySection from "@/components/money/budget/setting/CreateBudgetCategorySection";
 import CreateCategoryBudgetSection from "@/components/money/budget/setting/CreateCategoryBudgetSection";
+import { QueryKeys } from "@/utils/common/query-keys";
+import { useToastMessage } from "@/utils/hooks/chakra/useToastMessage";
+import { useMonthlyBudgetQuery } from "@/utils/hooks/react-query/money/useMonthlyBudgetQuery";
+import { createMontlyBudgetCategory } from "@/utils/services/money";
 import { BudgetCategory } from "@/utils/types/money";
 import {
   Modal,
@@ -12,6 +16,7 @@ import {
   ModalOverlay,
 } from "@chakra-ui/modal";
 import React, { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
 
 interface SettingCategoryBudgetModalProps {
   isOpen: boolean;
@@ -21,16 +26,53 @@ export default function CreateCategoryBudgetModal({
   isOpen,
   onClose,
 }: Readonly<SettingCategoryBudgetModalProps>) {
+  const queryClient = useQueryClient();
+  const { openToast } = useToastMessage();
+  const { data: monthlyBudget } = useMonthlyBudgetQuery();
+
   const [currentCategory, setCurrentCategory] = useState<BudgetCategory | null>(
     null,
   );
   const [budget, setBudget] = useState<string>("300000");
 
+  const resetState = () => {
+    setCurrentCategory(null);
+    setBudget("300000");
+  };
+
   const handleClose = () => {
+    resetState();
     onClose();
   };
 
-  const handleConfirm = () => {};
+  const { mutate: createMonthlyBudgetCategoryRequest } = useMutation(
+    ["create-monthly-budget-category"],
+    createMontlyBudgetCategory,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(
+          QueryKeys.getAllMonthlyBudgetCategoreis(),
+        );
+        handleClose();
+      },
+      onError: console.error,
+    },
+  );
+
+  const handleConfirm = () => {
+    if (!monthlyBudget || !currentCategory || !budget) {
+      return openToast({
+        status: "warning",
+        title: "생성 오류",
+        description: "필수 정보를 모두 입력해주세요.",
+      });
+    }
+    createMonthlyBudgetCategoryRequest({
+      monthlyBudgetId: monthlyBudget.id,
+      budgetCategoryId: currentCategory.id,
+      budget: +budget,
+    });
+  };
 
   return (
     <Modal

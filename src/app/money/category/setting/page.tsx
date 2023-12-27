a3adb347d880a7c51c87498fffeedb3d3b35794e
ckm960411 <a01098956117@gmail.com>
2023-12-28 01:13:26 +0900

    ✨ 카테고리 추가기능 구현

--- src/app/money/category/setting/page.tsx
+++ src/app/money/category/setting/page.tsx
@@ -2,11 +2,26 @@
 
 import BudgetCategoryCard from "@/components/money/budget/setting/BudgetCategoryCard";
 import BudgetCategorySettingCloseButton from "@/components/money/budget/setting/BudgetCategorySettingCloseButton";
+import { QueryKeys } from "@/utils/common/query-keys";
 import { useBudgetCategoriesQuery } from "@/utils/hooks/react-query/money/useBudgetCategoriesQuery";
+import { createBudgetCategory } from "@/utils/services/money";
 import React from "react";
+import { useMutation, useQueryClient } from "react-query";
 
 export default function CategorySettingPage() {
+  const queryClient = useQueryClient();
   const budgetCategories = useBudgetCategoriesQuery();
+
+  const { mutate: createBudgetCategoryRequest } = useMutation(
+    ["create-budget-category"],
+    createBudgetCategory,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.getAllBudgetCategories());
+      },
+      onError: console.error,
+    },
+  );
 
   return (
     <div>
@@ -21,7 +36,15 @@ export default function CategorySettingPage() {
             budgetCategory={budgetCategory}
           />
         ))}
-        <button className="w-full rounded-10px bg-main p-12px text-16px font-bold text-white shadow-lg">
+        <button
+          onClick={() =>
+            createBudgetCategoryRequest({
+              name: "카테고리명",
+              unicode: "1f4b5",
+            })
+          }
+          className="w-full rounded-10px bg-main p-12px text-16px font-bold text-white shadow-lg"
+        >
           카테고리 추가하기
         </button>
       </div>

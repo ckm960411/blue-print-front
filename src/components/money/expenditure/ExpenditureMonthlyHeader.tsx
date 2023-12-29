2302c1798b6f4dcb68edaf9d73c97d3614a73e12
ckm960411 <a01098956117@gmail.com>
2023-12-29 21:58:30 +0900

    ✨ year, month 수정 구현

--- src/components/money/expenditure/ExpenditureMonthlyHeader.tsx
+++ src/components/money/expenditure/ExpenditureMonthlyHeader.tsx
@@ -1,15 +1,32 @@
 import SpaceY from "@/components/common/SpaceY";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
-export default function ExpenditureMonthlyHeader() {
+interface ExpenditureMonthlyHeaderProps {
+  year: number;
+  month: number;
+  onChangeDate: (type: "prev" | "next") => void;
+}
+export default function ExpenditureMonthlyHeader({
+  year,
+  month,
+  onChangeDate,
+}: Readonly<ExpenditureMonthlyHeaderProps>) {
   return (
     <div>
       <div className="flex items-center gap-4px">
-        <button className="flex-center h-24px w-24px">
+        <button
+          onClick={() => onChangeDate("prev")}
+          className="flex-center h-24px w-24px"
+        >
           <FaChevronLeft />
         </button>
-        <p className="text-20px font-bold">2023년 12월</p>
-        <button className="flex-center h-24px w-24px">
+        <p className="text-20px font-bold">
+          {year}년 {month}월
+        </p>
+        <button
+          onClick={() => onChangeDate("next")}
+          className="flex-center h-24px w-24px"
+        >
           <FaChevronRight />
         </button>
       </div>
--- src/components/money/expenditure/ExpenditureTab.tsx
+++ src/components/money/expenditure/ExpenditureTab.tsx
@@ -1,10 +1,33 @@
+"use client";
+
 import ExpenditureListContainer from "@/components/money/expenditure/ExpenditureListContainer";
 import ExpenditureMonthlyHeader from "@/components/money/expenditure/ExpenditureMonthlyHeader";
+import { getMonth, getYear } from "date-fns";
+import { useState } from "react";
 
 export default function ExpenditureTab() {
+  const [year, setYear] = useState(getYear(new Date()));
+  const [month, setMonth] = useState(getMonth(new Date()) + 1);
+
+  const handleChangeDate = (type: "prev" | "next") => {
+    if (type === "prev") {
+      if (month !== 1) return setMonth((prev) => prev - 1);
+      setYear((prev) => prev - 1);
+      setMonth(12);
+    } else {
+      if (month !== 12) return setMonth((prev) => prev + 1);
+      setYear((prev) => prev + 1);
+      setMonth(1);
+    }
+  };
+
   return (
     <div>
-      <ExpenditureMonthlyHeader />
+      <ExpenditureMonthlyHeader
+        year={year}
+        month={month}
+        onChangeDate={handleChangeDate}
+      />
       <hr className="my-24px" />
       <ExpenditureListContainer />
     </div>

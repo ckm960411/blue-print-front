338cd8780768d189e2d904744efc5b28c0279762
ckm960411 <a01098956117@gmail.com>
2023-12-30 16:59:19 +0900

    ✨ Add 가격부분 추가

--- src/components/money/expenditure/CreateExpenditureModal.tsx
+++ src/components/money/expenditure/CreateExpenditureModal.tsx
@@ -33,6 +33,7 @@ export default function CreateExpenditureModal({
   const [minute, setMinute] = useState(getMinutes(now));
   const [type, setType] = useState<ExpenditureType>(ExpenditureType.SPENDING);
   const [category, setCategory] = useState<BudgetCategory | null>(null);
+  const [content, setContent] = useState("");
   const [price, setPrice] = useState(10000);
 
   const handleChangeDate = (type: "prev" | "next") => {
@@ -94,6 +95,15 @@ export default function CreateExpenditureModal({
                 hasNoneOption
               />
             )}
+            <div className="flex items-center gap-8px">
+              <span className="w-40px text-16px font-bold">설명 : </span>
+              <input
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                placeholder="ex) 슈퍼스타어반"
+                className="rounded-md border border-gray-200 px-6px py-6px text-16px"
+              />
+            </div>
             <div className="flex items-center gap-8px">
               <span className="w-40px text-16px font-bold">금액 : </span>
               <NumberInputController

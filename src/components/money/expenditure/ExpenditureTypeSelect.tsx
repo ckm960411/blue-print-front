5c68c365716fc02e61f9e4f2ff464eb9f02bb61e
ckm960411 <a01098956117@gmail.com>
2023-12-31 00:31:16 +0900

    💄 Update style

--- src/components/money/expenditure/ExpenditureTypeSelect.tsx
+++ src/components/money/expenditure/ExpenditureTypeSelect.tsx
@@ -19,28 +19,34 @@ export default function ExpenditureTypeSelect({
   });
 
   return (
-    <Menu>
-      <MenuButton
-        as={Button}
-        rightIcon={<IoChevronDownSharp />}
-        _expanded={{ bg: "#fff" }}
-        className="truncate-1-lines h-20px w-96px p-0px text-left text-14px"
-        style={{ display: "flex" }}
-      >
-        {expenditureType}
-      </MenuButton>
-      <MenuList>
-        {[ALL_EXPENDITURE, ...categoryNames].map((type) => (
-          <MenuItem
-            key={type}
-            onClick={() => onSelect(type)}
-            className="w-120px py-8px text-left text-14px"
-            _hover={{ bg: Colors.gray[50] }}
-          >
-            {type}
-          </MenuItem>
-        ))}
-      </MenuList>
-    </Menu>
+    <div className="flex-between">
+      <Menu>
+        <MenuButton
+          as={Button}
+          rightIcon={<IoChevronDownSharp />}
+          _expanded={{ bg: "#fff" }}
+          className="truncate-1-lines h-20px w-96px p-0px text-left text-14px"
+          style={{ display: "flex" }}
+        >
+          {expenditureType}
+        </MenuButton>
+        <MenuList>
+          {[ALL_EXPENDITURE, ...categoryNames].map((type) => (
+            <MenuItem
+              key={type}
+              onClick={() => onSelect(type)}
+              className="w-120px py-8px text-left text-14px"
+              _hover={{ bg: Colors.gray[50] }}
+            >
+              {type}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+      <div className="flex-center relative top-4px gap-8px">
+        <span className="w-80px text-end text-12px">수입</span>
+        <span className="w-80px text-end text-12px">지출</span>
+      </div>
+    </div>
   );
 }
